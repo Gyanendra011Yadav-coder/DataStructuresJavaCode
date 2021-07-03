846378eb86c200b21ca440cf6287d1ea577198ad
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-03 01:32:52 -0700

    Created The Class to write the code pof the stack.

---
+++ src/stackPrograms/stackUsingArray.java
@@ -0,0 +1,27 @@
+package stackPrograms;
+
+/**
+ *  Abhishek went for an interview at an IT company. The interviewer asks him to design a Data Structure that performs the Stack operation like push(), pop() and one more operation getMin(), getMin() function should return the minimum element from the stack. The interviewer also told him that all these operations must be in
+ * O
+ * (
+ * 1
+ * )
+ *  time and use only stack data structure.
+ *
+ *  Note: You have to answer the question according to query.
+ *     You will get four types of query.
+ *
+ * 1. push(x) - Push element x onto stack.
+ * 2. pop() - Removes the element on top of the stack.
+ * 3. top() - Get the top element.
+ * 4. getMin() - Get the minimum element in the stack.
+ *  Expected Time Complexity for each query is O(1)
+ *
+ * Note: The Interviewer did not allow you to use any other data structure except stack.
+ */
+
+public class stackUsingArray {
+
+}
+
+
