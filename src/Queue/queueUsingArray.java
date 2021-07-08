b5e375ec615aca759f7742829f068fc6389d96a5
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-07 18:47:41 -0700

    operations of enqueue, and dequeue in the Array.

--- src/Queue/queueUsingArray.java
+++ src/Queue/queueUsingArray.java
@@ -1,11 +1,23 @@
 package Queue;
 
 public class queueUsingArray {
-    int rear=-1,front=-1;
+    int rear=-1,front=-1;  // rear == back          front =begin
     int []queue;
     public queueUsingArray(int Size ){
         int [] queue=new int[Size];
     }
     /*ENQUEUE OPERATION IN THE QUEUE*/
 
+    void enqueue(int data){
+        if (rear==-1 && front==-1){
+            queue[0]=data;
+            rear++;
+            front++;
+        }else if (rear== queue.length-1){
+            System.out.println("QUEUE OVERFLOW.");
+        }else{
+            rear++;
+            queue[rear]=data;
+        }
+    }
 }
