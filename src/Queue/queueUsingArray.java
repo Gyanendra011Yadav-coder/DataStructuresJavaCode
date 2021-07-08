198038dfc09003f5c2b7671786b5ee896477c12b
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-07 18:52:58 -0700

    methods to check queue is empty is full or empty.

--- src/Queue/queueUsingArray.java
+++ src/Queue/queueUsingArray.java
@@ -6,6 +6,22 @@ public class queueUsingArray {
     public queueUsingArray(int Size ){
         int [] queue=new int[Size];
     }
+
+    boolean isQueueEmpty(){
+        if (rear==-1 &&front==-1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    boolean isQueueFull(){
+        if(rear== queue.length){
+            return true;
+        }else {
+            return false;
+        }
+    }
     /*ENQUEUE OPERATION IN THE QUEUE*/
 
     void enqueue(int data){
@@ -19,5 +35,8 @@ public class queueUsingArray {
             rear++;
             queue[rear]=data;
         }
+    }
+    void dequeue(){
+
     }
 }
