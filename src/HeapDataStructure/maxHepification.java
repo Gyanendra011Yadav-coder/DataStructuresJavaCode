ffba5037d457c2e81f7dbd2838d532b0f89e9582
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-12 21:08:33 -0700

    created the heap successfully.with it's constructor

---
+++ src/HeapDataStructure/maxHepification.java
@@ -0,0 +1,15 @@
+package HeapDataStructure;
+
+import java.util.Scanner;
+
+public class maxHepification {
+    int size;
+    int heap_size;
+    int []heap;
+    maxHepification(int heap_size){
+        this.heap_size=heap_size;
+        this.size=0;
+        heap=new int[heap_size];
+    }
+
+}
