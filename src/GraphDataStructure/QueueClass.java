e19e53f1b15a94473c3a0c951338ca9c4290d61f
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-18 07:49:50 -0700

    INITALIZED, THA VARIABLES USING CONSTRUCTOR.

--- src/GraphDataStructure/QueueClass.java
+++ src/GraphDataStructure/QueueClass.java
@@ -1,4 +1,18 @@
 package GraphDataStructure;
 
 public class QueueClass {
+    int []arr;    // WILL STORE THE VALUES
+    int front; // WILL BE POINTING THE FRONT
+    int rear; // WILL BE POINTING THE REAR
+    int count; // WILL BE USED TO CALACULATE THE CURRENT SIZE
+    int capacity; // USED TO CALCULATE THE CAPACITY OF WHOLE QUEUE
+
+    QueueClass(int size){
+        arr=new int[size];
+        front=0;
+        rear=-1;
+        count=0;
+        capacity=size;
+    }
+
 }
