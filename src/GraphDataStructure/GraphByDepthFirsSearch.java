4773196c59fd80430a4a214ca964e1848b528f6a
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-19 09:30:09 -0700

    created the variables and created the constructor of it.

--- src/GraphDataStructure/GraphByDepthFirsSearch.java
+++ src/GraphDataStructure/GraphByDepthFirsSearch.java
@@ -1,5 +1,15 @@
 package GraphDataStructure;
 
-public class GraphByDepthFirsSearch {
+import java.util.LinkedList;
 
+public class GraphByDepthFirsSearch {
+    private int vertex;
+    private LinkedList<Integer>[] adj;
+    @SuppressWarnings("unchecked")GraphByDepthFirsSearch (int size){
+        this.vertex=size;
+        adj=new LinkedList[size];
+        for (int i = 0; i <size ; i++) {
+            adj[i]=new LinkedList();   // creating the linked list ofn the Node ith.
+        }
+    }
 }
