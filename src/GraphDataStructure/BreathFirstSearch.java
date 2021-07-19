33708ef9d2d1e647280328575306fd8cae56e49c
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-19 06:06:05 -0700

    THIS IS THE METHOD THAT WILL TAKE CARE ABOUT THE BreathFirstSearch

--- src/GraphDataStructure/BreathFirstSearch.java
+++ src/GraphDataStructure/BreathFirstSearch.java
@@ -4,13 +4,14 @@ public class BreathFirstSearch {
     graphNode[] nodeList;
     int[][] adjMatrix;
     int numberOfNodes;
-
+    QueueClass queue;
     /*CONSTRUCTOR OF CLASS*/
     BreathFirstSearch(int size) {
         int MAX_SIZE = size;
         nodeList = new graphNode[MAX_SIZE];
         adjMatrix = new int[MAX_SIZE][MAX_SIZE];
         numberOfNodes = 0;
+        queue=new QueueClass(MAX_SIZE);
     }
 
     /*THIS, METHOD WILL INSERT THE VALUES IN THE GRAPH*/
@@ -43,6 +44,30 @@ public class BreathFirstSearch {
         }else{
             return -1;
         }
+    }
+
+    /*THIS, METHOD WILL APPLY THE BREADTH FIRST SEARCH APPROACH*/
+
+    public void breathForstSearch(){
+        /*
+           we are taking,0 that that shows that we are starting it from index, as it is Graph
+           we  can take any node to start.
+         */
+        nodeList[0].visited=true;
+   // printing the node
+        print(0);
+   // Enqueuing In the Queue
+        queue.enqueue(0);
+        int node1;
+        while (!queue.isEmpty()){
+            int node2= queue.dequeue();
+            while ((node1=unvisitedNode(node2))!=-1){
+                nodeList[node1].visited=true;
+                print(node1);
+                queue.enqueue(node1);
+            }
+        }
+
     }
 
 
