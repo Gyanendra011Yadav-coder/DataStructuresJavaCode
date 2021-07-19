1a6d3fba1f7c23ed30400f0a05ed40ec065159f5
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-19 06:19:04 -0700

    CALLED THE METHOD, addEdge, FOR ADDING THE EDGE, B/W THE GRAPH.

--- src/GraphDataStructure/graphMainClass.java
+++ src/GraphDataStructure/graphMainClass.java
@@ -11,7 +11,17 @@ public class graphMainClass {
         bg.addNode(30);
         bg.addNode(5);
         bg.addNode(300);
-        bg
+        bg.addEdge(0,1);
+        bg.addEdge(0,2);
+        bg.addEdge(1,4);
+        bg.addEdge(1,5);
+        bg.addEdge(2,3);
+        bg.addEdge(4,5);
+        bg.addEdge(4,6);
+        bg.addEdge(5,7);
+
+
+
 
 
 
