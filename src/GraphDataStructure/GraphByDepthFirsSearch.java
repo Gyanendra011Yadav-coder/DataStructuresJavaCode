15b6f94277c40da67eb26251e20095cab0513ec0
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-19 11:11:04 -0700

    Createdd the method where we will tell after what we are going to apply DFS On graph.
    and methdo tha will be called reursively.

--- src/GraphDataStructure/GraphByDepthFirsSearch.java
+++ src/GraphDataStructure/GraphByDepthFirsSearch.java
@@ -25,19 +25,54 @@ public class GraphByDepthFirsSearch {
     /*THIS FUNCTION WILL CREATE THE GRAPH AND WILL APPLY RECURSION */
 
     /**
-     * this mmethod takes: input as bertex that shows that we are STARTING IT FROM VERTEX TO ADD
+     * this method takes: input as bertex that shows that we are STARTING IT FROM VERTEX TO ADD
      *
      */
     public void depthFirstSearchNodeNotSpecified() {
         boolean[] visited = new boolean[vertex];
         for (int i = 0; i < vertex; i++) {
             if (visited[i]==false) {
-                //DFSUtil(i, visited);  //RECURSION
+                DFSUtil(i, visited);  //RECURSION
             }
         }
     }
+    public void depthFirstSearchNodeSpecified(int vertexAfterWhichDFSApplied){
+        boolean []visited = new boolean[vertex];
+        DFSUtil(vertexAfterWhichDFSApplied,visited);
+    }
+    /*THIS IS METHOD, THAT WILL BE CALLED FOR THE RECURSION*/
+    public void DFSUtil(int vertex, boolean[]visited){
+        visited[vertex]=true;
+        System.out.println(vertex);
+        //we, will check wheather all nodes are covered or not
+        Iterator<Integer> i = adj[vertex].listIterator();
+        while (i.hasNext()){
+            int n = i.next();
+            if (!visited[n]){
+                DFSUtil(n,visited);
+            }
+        }
+    }
+    /**
+     * this is the main method of this:-    GRAPH BY DFS
+     */
+
+    public static void main(String[] args) {
+        GraphByDepthFirsSearch gd = new GraphByDepthFirsSearch(4);
+        gd.addEdge(0,1);
+        gd.addEdge(0,2);
+        gd.addEdge(2,0);
+        gd.addEdge(2,3);
+        gd.addEdge(3,3);
+        System.out.println("NO NODE IS SPECIFIED:");
+        gd.depthFirstSearchNodeNotSpecified();
+        System.out.println("NODE SPECIFIED:-");
+        gd.depthFirstSearchNodeSpecified(2);
 
 
 
     }
+
+
+
 }
