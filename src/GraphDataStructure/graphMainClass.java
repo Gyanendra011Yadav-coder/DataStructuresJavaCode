17d6b69e8590ccdb4c3e781491f4c0e66df7f82e
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-19 06:16:46 -0700

    CREATED THE OBJECT OF BreathFirstSearch CLASS AND CALLING THE FUNCTION OF addNode for adding the node.

--- src/GraphDataStructure/graphMainClass.java
+++ src/GraphDataStructure/graphMainClass.java
@@ -2,6 +2,16 @@ package GraphDataStructure;
 
 public class graphMainClass {
     public static void main(String[] args) {
+        BreathFirstSearch bg = new BreathFirstSearch(8);
+        bg.addNode(50);
+        bg.addNode(10);
+        bg.addNode(200);
+        bg.addNode(20);
+        bg.addNode(15);
+        bg.addNode(30);
+        bg.addNode(5);
+        bg.addNode(300);
+        bg
 
 
 
