868d9bdedf3835627c7afef96367062047621b3e
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-22 09:52:19 -0700

    INITIALZED THE VECTOR AND ADDED THE COMMENT.

--- src/GraphDataStructure/RemoveEdgeRemoveCycle.java
+++ src/GraphDataStructure/RemoveEdgeRemoveCycle.java
@@ -1,11 +1,15 @@
 package GraphDataStructure;
 
+import java.util.Vector;
+
 /**
  * You are given the number of vertices(V) and the number of edges(E) of an undirected graph,
  * the task is to print the count of the minimum number of edges that must be removed from the
  * graph to break all of its cycles.
  */
 public class RemoveEdgeRemoveCycle {
-
+    //INITALIZED THE VECTOR;- VECTORS ARE DATA TYOES SUCK LIKE AS AN ARRAY THEY HAVE THE PROPERTY
+    // THAT THEY CAN SHRINK OR INCREASE THE SIZE ACCORDING TO THE NEED
+    Vector<Integer> vector= new Vector<>(100001);
 
 }
