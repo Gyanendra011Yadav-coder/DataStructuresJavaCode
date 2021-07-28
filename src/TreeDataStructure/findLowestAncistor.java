0914cff0c0cb18696f9109eb607a12e9a1a6297f
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-28 14:02:50 -0700

    CREATED THE NODE CLASS FOR THIS BST CLASS.

--- src/TreeDataStructure/findLowestAncistor.java
+++ src/TreeDataStructure/findLowestAncistor.java
@@ -1,7 +1,7 @@
 package TreeDataStructure;
 
 /****
- * IN THIS, WE WILL BE FNDING THE LOWEST COMMON ANCESTOR OF THE GIVEN NODES.
+ * IN THIS, WE WILL BE FINDING THE LOWEST COMMON ANCESTOR OF THE GIVEN NODES.
  * Ancestors
  * PrepBuddy is researching about different kinds of mammals and their ancestors.
  * He has assigned a unique integer number to each mammal and has arranged them in a binary search tree format. He needs your help to create a program that outputs lowest common ancestor of two mammals.
@@ -13,4 +13,13 @@ package TreeDataStructure;
  */
 
 public class findLowestAncistor {
+    class Node{
+        int data;
+        Node leftChild;
+        Node rightChild;
+        Node(int data){
+            this.data=data;
+            leftChild=rightChild=null;
+        }
+    }
 }
