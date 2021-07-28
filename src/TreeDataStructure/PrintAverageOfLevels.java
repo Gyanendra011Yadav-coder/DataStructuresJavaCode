f6366da033be419a9313153829bed8b6b9e22a5d
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-28 02:39:11 -0700

    chnaged the name of the variables as required.

--- src/TreeDataStructure/PrintAverageOfLevels.java
+++ src/TreeDataStructure/PrintAverageOfLevels.java
@@ -5,47 +5,50 @@ import java.util.Queue;
 
 public class PrintAverageOfLevels {
     static class Node{
-        int data;
-        Node leftChild;
-        Node rightChild;
+        long value;
+        Node left;
+        Node right;
         Node(int data){
-            this.data=data;
-            leftChild=rightChild=null;
+            this.value=data;
+            left = right =null;
         }
     }
     static Node root;
-    public static void levelOrder(Node root){
-        if(root==null){
+    public static void levelOrder(Node node){
+        if(node ==null){
             return ;
         }
         int sum=0, count=0;
         Queue <Node> q = new LinkedList<Node>();
-        q.add(root);
+        q.add(node);
         while (!q.isEmpty()){
+            sum=0;count=0;
             Queue<Node> tempQueue=new LinkedList<Node>();
             while (!q.isEmpty()){
                 Node tempNode=q.remove();
-                sum += tempNode.data;
+                sum += tempNode.value;
                 count++;
-                if(tempNode.leftChild!=null){
-                    tempQueue.add(tempNode.leftChild);
+                if(tempNode.left !=null){
+                    tempQueue.add(tempNode.left);
                 }
-                if (tempNode.rightChild!=null){
-                    tempQueue.add(tempNode.rightChild);
+                if (tempNode.right !=null){
+                    tempQueue.add(tempNode.right);
                 }
             }
+            q=tempQueue;
+            System.out.println(sum*1.0/count);
         }
 
     }
 
     public static void main(String[] args) {
         root = new Node(50);
-        root.leftChild = new Node(25);
-        root.rightChild = new Node(75);
-        root.leftChild.leftChild = new Node(15);
-        root.leftChild.rightChild = new Node(30);
-        root.rightChild.leftChild = new Node(70);
-        root.rightChild.rightChild = new Node(85);
+        root.left = new Node(25);
+        root.right = new Node(75);
+        root.left.left = new Node(15);
+        root.left.right = new Node(30);
+        root.right.left = new Node(70);
+        root.right.right = new Node(85);
         levelOrder(root);
 
 
