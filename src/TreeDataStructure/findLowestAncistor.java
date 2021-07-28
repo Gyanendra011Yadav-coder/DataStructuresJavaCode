1fc8ad78caee14f6dacf09266d16cdffe415ace8
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-28 14:01:13 -0700

    IN, THIS CLASS WE WILL BE FINDING THE LOWEST COMMON ANCESTOR OF THE GIVEN TWO NODES.

---
+++ src/TreeDataStructure/findLowestAncistor.java
@@ -0,0 +1,16 @@
+package TreeDataStructure;
+
+/****
+ * IN THIS, WE WILL BE FNDING THE LOWEST COMMON ANCESTOR OF THE GIVEN NODES.
+ * Ancestors
+ * PrepBuddy is researching about different kinds of mammals and their ancestors.
+ * He has assigned a unique integer number to each mammal and has arranged them in a binary search tree format. He needs your help to create a program that outputs lowest common ancestor of two mammals.
+ *
+ * Note
+ * -You have to complete findLowestAncestor() function, no need to implement the tree.
+ * Lowest common ancestor of two node n1 and n2 is the lowest node in the tree that has
+ * both n1 and n2 as descendents(where we allow a node to be a descendant of itself).
+ */
+
+public class findLowestAncistor {
+}
