4c51d02579043a89576e9be4444e5deabf5327f3
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-30 14:54:08 -0700

    CREATED THE NODE CLASS AND INTIALOZED THE CONSTRUTOR OF THE NODE CLASS.

--- src/TreeDataStructure/zigZagTraversalTree.java
+++ src/TreeDataStructure/zigZagTraversalTree.java
@@ -1,5 +1,14 @@
 package TreeDataStructure;
 
 public class zigZagTraversalTree {
+    class Node{
+        int data;
+        Node leftChild;
+        Node rightChild;
+        Node(int data){
+            this.data=data;
+        }
+    }
+
 
 }
