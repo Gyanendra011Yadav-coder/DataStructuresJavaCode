19fee985bcf99a5b15f35756cd0e84eff9172baf
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-30 11:37:24 -0700

    CREATED THE NODE CLASS OF THIS CLASS, IN TIS WE WILL BE DOING THE BOUNDARY WALL TRAVERSAL OF THE NODES, OF THE TREE.

---
+++ src/TreeDataStructure/boundaryTraversalTree.java
@@ -0,0 +1,14 @@
+package TreeDataStructure;
+
+public class boundaryTraversalTree {
+    static class Node{
+        int value;
+        mirrorTreesCheck.Node left;
+        mirrorTreesCheck.Node right;
+        Node (int data){
+            this.value=data;
+            left=right=null;
+        }
+    }
+
+}
