468939ac9ba72436f9622216d00ccdb4a01e67dd
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-30 11:50:33 -0700

    wrote the method that will be returning the leaves of the tree.

--- src/TreeDataStructure/boundaryTraversalTree.java
+++ src/TreeDataStructure/boundaryTraversalTree.java
@@ -3,12 +3,26 @@ package TreeDataStructure;
 public class boundaryTraversalTree {
     static class Node{
         int value;
-        mirrorTreesCheck.Node left;
-        mirrorTreesCheck.Node right;
+        Node left;
+        Node right;
         Node (int data){
             this.value=data;
             left=right=null;
         }
+    }
+
+    /***
+     *IN, THIS METHOD WE WILL BE PRINTING THE LEAVES OF THE TREE
+     */
+    public static void isLeave(Node node ){
+        if(node==null){
+            return;
+        }
+        isLeave(node.left); // THIS, WILL BE CHECKING THE LEAVES OF THE LEFT SUB-TREE
+        if (node.left==null && node.right == null){  // HERE,WE ARE CHECKING IF THE LEFT AND RIGHT OF ANY NODE IS NULL THEN WE WILL BE PRINT ING IT AS AN LEAVE OF THE TREE
+            System.out.println(node.value + " ");
+        }
+        isLeave(node.right);  // THIS, WILL BE CHECKING THE RIGHT SUB TREE
     }
 
 }
