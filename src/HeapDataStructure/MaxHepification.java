833d94c2b1959558672ebc126d8d1527567b14a8
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-31 12:15:02 -0700

    CCREATD THE 3 METHODS, 1) WILL BE BUILDING THE HEAP
    2) WILL BE CHECKING IF THE NODE IS LEAVE NODE OR NON LEAVE NODE
    
    3)WILL BE MAKING THE MAX HEAP.

--- src/HeapDataStructure/MaxHepification.java
+++ src/HeapDataStructure/MaxHepification.java
@@ -16,6 +16,35 @@ public class MaxHepification {
     public void insert(int data){
         heap[++size] = data;
     }
+    /*****
+     * In, this method we will be Converting the heap to Max_Heap
+     */
+    public void build_Heap(){
+        int lastNonLeaveNode= (int) Math.floor(size/2.0); // BY, USING THS WE ARE FINDING THE LAST NON-LEAVE NODES
+// THIS,LOOP WILL BE ITERATING FROM THE LAST NON-LEAVE NODE TO 1ST NON-LEAVE NODE
+        for (int i =lastNonLeaveNode; i>1 ; i--) {
+            max_Heap(heap,i);
+        }
+    }
+
+    /*****
+     *THIS, WILL BE CONVERTING THE HEAP TO THE MAX HEAP
+     */
+    private void max_Heap(int[] heap, int i) {
+        if (isLeaf(i)) {
+            return;
+        }
+    }
+
+    private boolean isLeaf(int positionOfTheNode) {
+ // HENCE, OUR LEAVE EXISTS,BETWEEN (SIZE/2)+1 TO SIZE
+        if (positionOfTheNode>(size)/2 && positionOfTheNode<size){
+            return true;
+        }else{
+            return false;
+
+        }
+    }
 
 
 }
