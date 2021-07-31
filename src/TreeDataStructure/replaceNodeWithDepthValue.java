d9145ea1673252b53b7c81a0fcfab1ac24705dd9
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-30 20:08:57 -0700

    INTIALIZED THE NODE CLASS OF THE METHOD.

---
+++ src/TreeDataStructure/replaceNodeWithDepthValue.java
@@ -0,0 +1,14 @@
+package TreeDataStructure;
+
+public class replaceNodeWithDepthValue {
+    Node rootNode;
+    class Node {
+        Node leftChild;
+        Node rightChild;
+        int data;
+        Node(int value){
+            this.data = value;
+            leftChild=rightChild=null;
+        }
+    }
+}
