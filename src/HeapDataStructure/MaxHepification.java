ab1f8cba4d113adc700f4ba91e6bebaf70810b79
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-31 11:38:10 -0700

    CREATED THE REQUIRED VARIABLES TO STORE THE ELEMENT OF THE HEAP, AND TO STORE THE SIZEE OF THE HEAP.

--- src/HeapDataStructure/MaxHepification.java
+++ src/HeapDataStructure/MaxHepification.java
@@ -1,2 +1,15 @@
-package HeapDataStructure;public class MaxHepification {
+package HeapDataStructure;
+
+public class MaxHepification {
+    int []heap; // This, is the heap Array that will be storing the nodes of the tree
+    int maxHeapSize;   // this will be storing the total Length of the heap
+    int size;         // this will storing the no of nodes inserted in the heap
+    MaxHepification(int size){
+        maxHeapSize = size;
+        size=0;
+        heap=new int[size];
+        heap[0] = Integer.MAX_VALUE;
+    }
+    
+
 }
--- src/HeapDataStructure/maxHepification.java
+++
@@ -1,5 +0,0 @@
-package HeapDataStructure;
-
-public class MaxHepification {
-
-}
