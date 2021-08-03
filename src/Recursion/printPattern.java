dc9b57baa0dfccbbd3cd9d625432618119bd98e2
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-08-03 13:51:27 -0700

    WROTE THE ITERATIVE APPROACH FOR PRINTING THE PATTERN.

---
+++ src/Recursion/printPattern.java
@@ -0,0 +1,23 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class printPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N=sc.nextInt();
+        int temp=N;
+        System.out.print(temp+" ");
+        while(N>0){
+            //N-=5;
+            N=N-5;
+            System.out.print(N+" ");
+
+        }
+        while(N!=temp){
+            N=N+5;
+            System.out.print(N+" ");
+        }
+        //while(N!=16){}
+    }
+}
