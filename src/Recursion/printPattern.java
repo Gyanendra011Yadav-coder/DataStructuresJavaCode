dcc29e1832ebf4c81b434e33cf72baa4429dec3b
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-08-03 14:22:51 -0700

    WROTE THE RECURSIVE APPROACH FOR PRITING THE PATTERN.

--- src/Recursion/printPattern.java
+++ src/Recursion/printPattern.java
@@ -3,12 +3,22 @@ package Recursion;
 import java.util.Scanner;
 
 public class printPattern {
-    public void printPatterns(int N){
+    public static int printPatterns(int N){
+        int temp=N;
+        while (N>0){
+            return N-printPatterns(N-5);
+        }
+        while (N!=temp){
+            return N+printPatterns(N+5);
+        }
 
+    return 0;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N=sc.nextInt();
+        System.out.print(printPatterns(N)+" ");
+        /*
         int temp=N;
         System.out.print(temp+" ");
         while(N>0){
@@ -21,5 +31,7 @@ public class printPattern {
             System.out.print(N+" ");
         }
         //while(N!=16){}
+        */
+
     }
 }
