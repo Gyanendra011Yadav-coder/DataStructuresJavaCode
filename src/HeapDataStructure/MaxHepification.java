efd9cb5b5f44ac7e67d26700ff2292bfa956f0e4
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-08-02 21:49:28 -0700

    ADDED THE SOME OTHER TECHNIQUES FOR PRINTING THE MAX HEAP.

--- src/HeapDataStructure/MaxHepification.java
+++ src/HeapDataStructure/MaxHepification.java
@@ -5,29 +5,31 @@ public class MaxHepification {
     int maxHeapSize;   // this will be storing the total Length of the heap
     int size;         // this will storing the no of nodes inserted in the heap
     MaxHepification(int size){  //constructor of the class
-        maxHeapSize = size;
+        this.maxHeapSize = size;
         size=0;
-        heap=new int[maxHeapSize+1];
-        heap[0] = Integer.MAX_VALUE;
+        heap=new int[this.maxHeapSize+1];
+        heap[0] = Integer.MAX_VALUE;  // HERE, WE ARE MAKING THE VALUE AT THE INDEX 0, AS WE DO NOT USE 0 INDEX IN THE HEAPS
     }
     /******
      * IN, THIS METHOD WE WILL BE ADDING THE NODES IN THE HEAP
      */
     public void insert(int data){
         heap[++size] = data;
+        /**
         int index=size;
         while(index>1 && heap[index/2]>heap[index]){
             swap(index,index/2);
             index=index/2;
+         */
         }
-    }
+
     /*****
      * In, this method we will be Converting the heap to Max_Heap
      */
     public void build_Heap(){
         int lastNonLeaveNode= (int) Math.floor(size/2.0); // BY, USING THS WE ARE FINDING THE LAST NON-LEAVE NODES
 // THIS,LOOP WILL BE ITERATING FROM THE LAST NON-LEAVE NODE TO 1ST NON-LEAVE NODE
-        for (int i =lastNonLeaveNode; i>=1 ; i--) {
+        for (int i=lastNonLeaveNode; i>=1 ; i--) {
             max_Heap(heap,i);
         }
     }
@@ -41,6 +43,31 @@ public class MaxHepification {
         }
         int leftChild=2*i;
         int rightChild=(2*i)+1;
+        /*
+        if (rightChild<=size ){
+            if(heap[leftChild]<=heap[i] && heap[rightChild] <=heap[i]){
+                return;
+            }
+        }else{
+            if (heap[leftChild]<=heap[i]){
+                return;
+            }
+        }
+        int largest;
+        if (leftChild<=size && heap[leftChild]>heap[i])
+        {
+            largest = leftChild;
+        }else {
+            largest=i;
+        }
+        if (rightChild<=size && heap[rightChild]>heap[largest]){
+            largest = rightChild;
+        }
+        if(i!=largest){
+            swap(i,largest);
+        }
+        max_Heap(heap, largest);
+        */
        if(leftChild<maxHeapSize && heap[i]<heap[leftChild]){
            if(heap[leftChild]>heap[rightChild]){
                swap(i,leftChild);
@@ -58,7 +85,9 @@ public class MaxHepification {
                max_Heap(heap, rightChild);
            }
         }
+
     }
+    
 
     /******
      *THIS, METHOD WILL BE SWAPING THE VALUES
@@ -74,7 +103,7 @@ public class MaxHepification {
      * */
     public boolean isLeaf(int positionOfTheNode) {
  // HENCE, OUR LEAVE EXISTS,BETWEEN (SIZE/2)+1 TO SIZE
-        if (positionOfTheNode>(size)/2 && positionOfTheNode<size){
+        if (positionOfTheNode>(size/2) && positionOfTheNode<=size){
             return true;
         }else{
             return false;
