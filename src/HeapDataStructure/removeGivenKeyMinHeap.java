c182719a4c21141cf3c0d46203b25bd101588110
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-08-07 16:36:17 +0530

    CREATED THE MIN HEAP IN THE CLASS.

---
+++ src/HeapDataStructure/removeGivenKeyMinHeap.java
@@ -0,0 +1,14 @@
+package HeapDataStructure;
+
+public class removeGivenKeyMinHeap {
+    int []heap;
+    int size;
+    int maxSize;
+    removeGivenKeyMinHeap(int size){
+        heap=new int[size];
+        size=0;
+        maxSize = size;
+    }
+
+}
+
