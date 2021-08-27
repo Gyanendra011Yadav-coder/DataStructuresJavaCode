8ddebc4a3a3ae411ec517da22ef2a8fea843aa19
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-08-27 23:11:07 +0530

    cREATD THE GRAPH CLASS AND INTILAIZED THE VARIABLES THAT ARE REQUIRED IN IT.

--- src/GraphDataStructure/breadthFirstSearch.java
+++ src/GraphDataStructure/breadthFirstSearch.java
@@ -1,5 +1,8 @@
 package GraphDataStructure;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class breadthFirstSearch {
     //THIS IS THE NODE CLASS OF THE GRAPH.
    class Node {
@@ -9,6 +12,20 @@ public class breadthFirstSearch {
            this.data = data;
            visited = false;
        }
-
    }
+
+   class Graph{
+       int noNodesInserted;
+       Node nodeList[];
+       int adjMatrix[][];
+       Queue<Integer> queue = new LinkedList<>();
+       Graph(int length){
+            int max_Length=length;
+            noNodesInserted=0;
+            nodeList = new Node[max_Length];
+            adjMatrix=new int[max_Length][max_Length];
+       }
+   }
+
+
 }
