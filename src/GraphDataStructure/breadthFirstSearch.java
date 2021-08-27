7afc0075f8523ee10d13dd78bfeea6aacc4d0c96
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-08-27 14:38:09 +0530

    CREATED THE NODE CLASS OF THE GRAPH.

--- src/GraphDataStructure/breadthFirstSearch.java
+++ src/GraphDataStructure/breadthFirstSearch.java
@@ -1,5 +1,14 @@
 package GraphDataStructure;
 
 public class breadthFirstSearch {
-   
+    //THIS IS THE NODE CLASS OF THE GRAPH.
+   class Node {
+       int data;
+       boolean visited;
+       Node(int data){
+           this.data = data;
+           visited = false;
+       }
+
+   }
 }
