d8d065b170e6f7830d2d20263b49be8e03cdef4d
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-08-30 19:38:28 +0530

    created the constructor, of the Main class  & new method to add node in the graph.

--- src/GraphDataStructure/depthFirstSearch.java
+++ src/GraphDataStructure/depthFirstSearch.java
@@ -9,6 +9,24 @@ public class depthFirstSearch {
          visited=false;
      }
  }
+ //REQUIRED COMPONENTS FOR THE TREE
+ int noNodesInserted;
+ int [][]adjMatrix;
+ Node []nodeList;
+ depthFirstSearch(int size){
+     int max_Size=size;
+     noNodesInserted=0;
+     adjMatrix= new int[max_Size][max_Size];
+     nodeList= new Node[max_Size];
+ }
+//method to add new  nodes in the Graph
+    public void addVertex(int data){
+     nodeList[noNodesInserted++]=new Node(data);
+    }
+
+    //method to add edges
+
+
 
 }
 
