f35125a7f963e904c4d1963a33aa6efd093bbfb8
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-08-30 18:46:18 +0530

    created the node class for the graph.

--- src/GraphDataStructure/depthFirstSearch.java
+++ src/GraphDataStructure/depthFirstSearch.java
@@ -1,7 +1,14 @@
 package GraphDataStructure;
 
 public class depthFirstSearch {
-
+ class Node{
+     int data;
+     boolean visited;
+     Node(int data){
+         this.data=data;
+         visited=false;
+     }
+ }
 
 }
 
