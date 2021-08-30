904677cc41fb5aeb79cd39af0f9b87ebe63f3176
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-08-30 23:19:45 +0530

    WROTE THE CODE OF THE MAIN CLASS AND PASSED THE NODES OF THE GRAPH.

--- src/GraphDataStructure/depthFirstSearch.java
+++ src/GraphDataStructure/depthFirstSearch.java
@@ -20,7 +20,7 @@ public class depthFirstSearch {
      nodeList= new Node[max_Size];
  }
 //method to add new  nodes in the Graph
-    public void addVertex(int data){
+    public void addNode(int data){
      nodeList[noNodesInserted++]=new Node(data);
     }
 
@@ -33,7 +33,7 @@ public void addEdge(int start, int end) {
 
 //METHOD TO PRINT THE NODES OF THE GRAPH
     public void printNodes(int index){
-      System.out.println(nodeList[index].data+" ");
+      System.out.print(nodeList[index].data+" ");
  }
 
  //method to check if the node is visited or not
@@ -54,6 +54,30 @@ public void addEdge(int start, int end) {
       if((node1=isVisited(n))!=-1){
           dfs(node1);
         }
+    }
+    //this is the main method of the class
+    public static void main(String[]args){
+     depthFirstSearch d = new depthFirstSearch(8);
+        d.addNode(50);
+        d.addNode(200);
+        d.addNode(20);
+        d.addNode(10);
+        d.addNode(15);
+        d.addNode(30);
+        d.addNode(5);
+        d.addNode(300);
+
+        d.addEdge(0, 3);
+        d.addEdge(0,1);
+        d.addEdge(1, 2);
+        d.addEdge(3, 4);
+        d.addEdge(3, 5);
+        d.addEdge(4, 5);
+        d.addEdge(4, 6);
+        d.addEdge(5, 7);
+        d.addEdge(5, 6);
+        d.addEdge(6, 7);
+        d.dfs(0);
     }
 
 
