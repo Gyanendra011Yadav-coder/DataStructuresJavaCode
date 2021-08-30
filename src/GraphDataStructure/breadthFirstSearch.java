17f984044d6547085d025ae30a590b2f621acd4e
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-08-30 09:53:18 +0530

    WROTE THE WHOLE CODE OF BREADTH FIRST SEARCH, IN THE TREES.

--- src/GraphDataStructure/breadthFirstSearch.java
+++ src/GraphDataStructure/breadthFirstSearch.java
@@ -1,55 +1,85 @@
 package GraphDataStructure;
-import java.util.LinkedList;
+
 import java.util.Queue;
 
 public class breadthFirstSearch {
-    //THIS IS THE NODE CLASS OF THE GRAPH.
-   class Node {
-       int data;
-       boolean visited;
-       Node(int data){
-           this.data = data;
-           visited = false;
-       }
-   }
+    class Node {
+        int data;
+        boolean visited;
+        Node(int data){
+            this.data=data;
+            visited = false;
+        }
+    }
+    int noNodeCount;
+    int adjMatrix[][];
+    Node nodeList[];
+    Queue<Integer> queue=new java.util.LinkedList<>();
+    breadthFirstSearch(int size){
+        int max_Size=size;
+        noNodeCount=0;
+        nodeList=new Node[max_Size];
+        adjMatrix=new int [max_Size][max_Size];
+    }
 
-   class Graph{
-       int noNodesInserted;
-       Node nodeList[];
-       int adjMatrix[][];
-       Queue<Integer> queue = new LinkedList<>();
-       Graph(int length){
-            int max_Length=length;
-            noNodesInserted=0;
-            nodeList = new Node[max_Length];
-            adjMatrix=new int[max_Length][max_Length];
-       }
+    public void addNode(int data){
+        nodeList[++noNodeCount]=new Node(data);
+    }
 
-       /***
-        *  THIS,METHOD WILL BE ADDING THE NODES IN THE NODELIST.
-        */
-       public void insertNodeGraph(int element){
-           nodeList[++noNodesInserted]=new Node(element);
-       }
+    public void addEdge(int start, int end){
+        adjMatrix[start][end]=1;
+        adjMatrix[end][start]=1;
+    }
 
-       /**
-        * THIS,METHOD WILL BE ADDING THE EDGES B/W THE VERTICES
-        */
-       public void addEdge(int start, int end){
-           adjMatrix[start][end]=1;
-   //   THIS,STATEMENT WE WILL ADD WHEN IT IS UN-DIRECTED GRAPH.
-           adjMatrix[end][start]=1;
-       }
-       /**
-        * this method wil be used to print the Nodes of  the graph.
-        */
-       public void printNode(int index){
-           System.out.println(nodeList[index].data);
-       }
-   }
+    public void printNode(int index){
+        System.out.println(nodeList[index].data+" ");
+    }
 
+    public int isConnected(int index){
+        for (int i = 0; i <noNodeCount; i++) {
+            if (adjMatrix[index][i]== 1 && nodeList[index].visited==false){
+                return i;
+            }
+        }
+        return -1;
+    }
 
+    void bfs() {
+        nodeList[0].visited = true;
+        printNode(0);
+        queue.add(0);
+        int node1;
+        while (!queue.isEmpty()) {
+            int node2 = queue.poll();
+            while ((node1 = isConnected(node2)) != -1) {       // if the node is not yet visited then mark it as visited and then print it and store the index value to traverse the adjacent node
+                nodeList[node1].visited = true;
+                printNode(node1);
+                queue.add(node1);
+            }
+        }
+    }
 
+    public static void main(String[] args) {
+        breadthFirstSearch BFS = new breadthFirstSearch(8);
+        BFS.addNode(50);
+        BFS.addNode(200);
+        BFS.addNode(20);
+        BFS.addNode(10);
+        BFS.addNode(15);
+        BFS.addNode(30);
+        BFS.addNode(5);
+        BFS.addNode(300);
 
+        BFS.addEdge(0, 3);
+        BFS.addEdge(1, 2);
+        BFS.addEdge(3, 4);
+        BFS.addEdge(3, 5);
+        BFS.addEdge(4, 5);
+        BFS.addEdge(4, 6);
+        BFS.addEdge(5, 7);
+        BFS.addEdge(5, 6);
+        BFS.addEdge(6, 7);
+        BFS.bfs();
+    }
+    }
 
-}
