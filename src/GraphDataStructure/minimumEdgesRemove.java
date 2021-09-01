184ea521676181d25ce34a85926ab90df6e7e1a5
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-09-01 22:44:55 +0530

    added the mthod to find out the minimum no of edges should be removed to remove cycles.

--- src/GraphDataStructure/minimumEdgesRemove.java
+++ src/GraphDataStructure/minimumEdgesRemove.java
@@ -1,5 +1,11 @@
 package GraphDataStructure;
 
+import java.util.Scanner;
+
 public class minimumEdgesRemove {
+  public int minEdge(int vertex, int edge){
+      return (vertex-edge)-1;
+  }
+
 
 }
