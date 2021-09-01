0556914ba14fbceb9e093a2593015766edf9198f
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-09-02 00:09:35 +0530

    CREATED THE LINKED LIST AT EVERY VERTEX OF THE GRAPH.

--- src/GraphDataStructure/totalComponents.java
+++ src/GraphDataStructure/totalComponents.java
@@ -1,5 +1,14 @@
 package GraphDataStructure;
 
-public class totalComponents {
+import java.util.LinkedList;
 
+class totalComponents {
+     int vertex;
+     LinkedList<Integer> adj[];
+     totalComponents(int size){
+         vertex=size;
+         for (int i = 0; i <size ; i++) {
+            adj[i] = new LinkedList<Integer>();
+         }
+     }
 }
