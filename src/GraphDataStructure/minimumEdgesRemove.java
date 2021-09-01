2282cce10fc4a4904c6fb72a7543c33877622988
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-09-01 22:47:24 +0530

    added the main method of the class.

--- src/GraphDataStructure/minimumEdgesRemove.java
+++ src/GraphDataStructure/minimumEdgesRemove.java
@@ -7,5 +7,13 @@ public class minimumEdgesRemove {
       return (vertex-edge)-1;
   }
 
+    public static void main(String[] args) {
+      minimumEdgesRemove min = new minimumEdgesRemove();
+        Scanner sc = new Scanner(System.in);
+        int vertex = sc.nextInt();
+        int edges = sc.nextInt();
+        int result=min.minEdge(vertex,edges);
+        System.out.println(result);
+    }
 
 }
