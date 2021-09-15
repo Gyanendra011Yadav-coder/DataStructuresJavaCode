bf1c029850a8b2015abf3b737e580dd7a782f6bb
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-09-15 15:41:39 +0530

    wrote the basic statments to check wheather the element is find at the starting of the array or at the end.

--- src/DivideAndConquer/findNearestElement.java
+++ src/DivideAndConquer/findNearestElement.java
@@ -1,4 +1,20 @@
 package DivideAndConquer;
 
+import java.util.Scanner;
+
 public class findNearestElement {
+    public int findNearestElement(int []array, int elementCompare){
+        int low=0, high=array.length-1;
+        if (array[0]>elementCompare){
+            return array[0];
+        }
+        else if(elementCompare>array[high]){
+            return array[high];
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+    }
 }
