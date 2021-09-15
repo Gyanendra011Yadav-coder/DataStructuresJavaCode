81db49310ff3c3a049cdd0d7344e430f3fcf69b7
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-09-15 17:28:48 +0530

    intialized the variable to to check from 1 to half of the number. and then if on multipying if the value is becoming equal to the number than we wull bw rwtutongin the same

--- src/DivideAndConquer/SquareRoot.java
+++ src/DivideAndConquer/SquareRoot.java
@@ -5,7 +5,13 @@ import java.util.Scanner;
 public class SquareRoot {
 
     public static int findSquareRoot(int number){
+        int index=1;
+        while(index<number/2){
+            if(index * index == number){
 
+            }
+        }
+        return 0;
     }
 
     public static void main(String []args){
