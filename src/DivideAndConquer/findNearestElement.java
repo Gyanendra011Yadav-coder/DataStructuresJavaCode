a37a9d6ff786bde21cd8f80d33b632a2b6f59f40
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-09-15 16:54:17 +0530

    wrote the code here we are checking if  the element element to which is to be compared should be smaller than mid then we will be traversing the first half of the array if the mid is greater than the value then we will be traversing the second half.

--- src/DivideAndConquer/findNearestElement.java
+++ src/DivideAndConquer/findNearestElement.java
@@ -3,16 +3,30 @@ package DivideAndConquer;
 import java.util.Scanner;
 
 public class findNearestElement {
-    public int findNearestElement(int []array, int elementCompare){
-        int low=0, high=array.length-1;
-        if (array[0]>elementCompare){
+    public int findNearestElement(int []array, int elementCompare) {
+        int low = 0, high = array.length - 1;
+        int mid = low + high / 2;
+        if (array[0] > elementCompare) {
             return array[0];
-        }
-        else if(elementCompare>array[high]){
+        } else if (elementCompare > array[high]) {
             return array[high];
+        } else if (array[mid] == elementCompare) {
+            return array[mid];
+        } else if (array[mid] > elementCompare) {
+            if (mid > 0 && array[mid - 1] < elementCompare) {
+                return min(array[mid-1],array[mid],elementCompare );
+            }
         }
-
+        return 0;
     }
+
+        public int min(int value1, int value2, int target){
+            if(target-value1>=value2-target){
+                return value2;
+            }else
+                return value1;
+
+        }
 
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
