f4d8dc5ffcebbfe821f6b20f538d31639ee15447
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-09-15 17:24:44 +0530

    CREATED THIS METHOD TO FIND OUT THE SQUARE ROOT OF THE NUMBER.

---
+++ src/DivideAndConquer/SquareRoot.java
@@ -0,0 +1,12 @@
+package DivideAndConquer;
+
+import java.util.Scanner;
+
+public class SquareRoot {
+
+
+    public static void main(String []args){
+        Scanner sc= new Scanner(System.in);
+        int numberFindSquareRoot = sc.nextInt();
+    }
+}
