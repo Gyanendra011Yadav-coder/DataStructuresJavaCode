80fdaa3b1fc9a80e792aa36979d55c07601fa31c
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-09-16 16:16:17 +0530

    writing the main method of the class and intialized the variable to take input by the user.

--- src/DivideAndConquer/SquareRoot.java
+++ src/DivideAndConquer/SquareRoot.java
@@ -1,13 +1,15 @@
 package DivideAndConquer;
 
+import java.util.Scanner;
+
 public class SquareRoot {
 
 //this is the optimised approach, that we wil be using to Find,Square root Using, Binary Search ny dividiing it
 
-    public static int findSquareRoot(int number) {
+    public static long findSquareRoot(int number) {
         int begin = 1;
         int end = number;
-        long answer;
+        long answer=0;
         while (begin < end) {
             int mid = number / 2;
             if (number == 0 || number == 1) {
@@ -21,7 +23,12 @@ public class SquareRoot {
                 end=mid-1;
             }
         }
-        return 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number=sc.nextInt();
     }
 
 }
@@ -30,6 +37,7 @@ public class SquareRoot {
 
 
     /*
+
     public static int findSquareRoot(int number){
         if (number== 0) {
             return 0;
