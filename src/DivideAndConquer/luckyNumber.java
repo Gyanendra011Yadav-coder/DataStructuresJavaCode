45f6878755745017745f60b83229fb1b8f15ec73
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-09-29 16:30:42 +0530

    Added the Method, To Find out the Number.

--- src/DivideAndConquer/luckyNumber.java
+++ src/DivideAndConquer/luckyNumber.java
@@ -4,7 +4,18 @@ public class luckyNumber {
     public void findNumber(long lN1, long lN2, int k){
         long low=0;
          long high=Long.MAX_VALUE;
-         long mid= (low+high)/2;
+
+         long answer;
+        while(low<high){
+            long mid= (low+high)/2;
+            answer=mid/lN1+mid/lN2-mid/gcd(lN1,lN2);
+            if(answer<k){
+                low=mid+1;
+            }else{
+                high=mid;
+            }
+            System.out.println(low);
+        }
 
     }
     public static long gcd(long a , long b){
