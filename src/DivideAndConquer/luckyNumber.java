321608e3e9c891be42a690385f9428766f361766
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-09-29 16:22:12 +0530

    Wroitting the code for Finding out the Lucky Number, by Dividing the Number that is provided.

---
+++ src/DivideAndConquer/luckyNumber.java
@@ -0,0 +1,10 @@
+package DivideAndConquer;
+
+public class luckyNumber {
+    public void findNumber(long lN1, long lN2, int k){
+        long low=0;
+         long high=Long.MAX_VALUE;
+         long mid= (low+high)/2;
+
+    }
+}
