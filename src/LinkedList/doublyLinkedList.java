b6cfbf1cc9014b4983fa9b53c548ee40aff07585
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2021-11-08 19:00:43 +0530

    Created The Node of the List.

--- src/LinkedList/doublyLinkedList.java
+++ src/LinkedList/doublyLinkedList.java
@@ -2,4 +2,20 @@ package LinkedList;
 
 public class doublyLinkedList {
 
+    //Node Class Of The Linked List
+    class NodeList{
+        //this will be storing the value in the Node.
+        int data;
+        //This will be pointing to the next Node
+        NodeList next;
+        //This will be pointing to the previous Node
+        NodeList previous;
+        //This is the Constructor Of The Node
+        NodeList(int data){
+            this.data=data;
+            next=null;
+            previous=null;
+        }
+    }
+
 }
