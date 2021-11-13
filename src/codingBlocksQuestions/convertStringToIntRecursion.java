76dbe96c564ffce66b4cb2aa613411a1fbbded98
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2021-11-13 12:56:36 +0530

    Wrote main method of the class and also have intialized the method.

--- src/codingBlocksQuestions/convertStringToIntRecursion.java
+++ src/codingBlocksQuestions/convertStringToIntRecursion.java
@@ -1,7 +1,22 @@
 package codingBlocksQuestions;
 
+import java.util.Scanner;
+
 public class convertStringToIntRecursion {
 
+
+    public static String returnNumberAsString(String number){
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine(); // this will be taking the user's input of an number as an String
+        String result=returnNumberAsString(str);
+        System.out.println(result);
+
+    }
 
 
 }
