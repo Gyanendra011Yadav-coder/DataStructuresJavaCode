b84d0f112514af5167923ab2c3ba9f4826d5099a
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2021-11-16 22:38:04 +0530

    Wrote the Main method of the class to Solve the problem.

--- src/codingBlocksQuestions/returnIndex.java
+++ src/codingBlocksQuestions/returnIndex.java
@@ -1,5 +1,7 @@
 package codingBlocksQuestions;
 
+import java.util.Scanner;
+
 /**
  * Take as input N, the size of array. Take N more inputs and store that in an array.
  * Take as input M, a number. Write a recursive function which returns an array containing
@@ -7,4 +9,13 @@ package codingBlocksQuestions;
  * which have value equal to M. Print all the values in the returned array.
  */
 public class returnIndex {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arrayLength = sc.nextInt();
+        int []array=new int[arrayLength];
+        for (int i = 0; i <arrayLength; i++) {
+            array[i]=sc.nextInt();
+        }
+    }
 }
