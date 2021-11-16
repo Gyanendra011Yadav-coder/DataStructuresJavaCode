bf62dab35f3df741fbc94ad7ef6d4f3404bae3b7
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2021-11-16 22:36:07 +0530

    Trying to Solve another Probelm.

---
+++ src/codingBlocksQuestions/returnIndex.java
@@ -0,0 +1,10 @@
+package codingBlocksQuestions;
+
+/**
+ * Take as input N, the size of array. Take N more inputs and store that in an array.
+ * Take as input M, a number. Write a recursive function which returns an array containing
+ * indices of all items in the array
+ * which have value equal to M. Print all the values in the returned array.
+ */
+public class returnIndex {
+}
