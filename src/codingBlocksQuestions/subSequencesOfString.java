86649041699729e9b8259a952aaaa606ed82811b
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2021-11-18 01:14:28 +0530

    Wrote the method to write the method to print the sub-Strings.

--- src/codingBlocksQuestions/subSequencesOfString.java
+++ src/codingBlocksQuestions/subSequencesOfString.java
@@ -4,13 +4,24 @@ import java.util.Scanner;
 
 public class subSequencesOfString {
 
-    public static void main(String []args){
-        Scanner sc= new Scanner(System.in);
+    public static void main(String[] args) {
+        subSequencesOfString sub = new subSequencesOfString();
+        Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-        String result=printSubsequnces(str);
+        sub.generate(str);
     }
 
-    private static String printSubsequnces(String str) {
+    private void generate(String word) {
+        int count=0;
+        if (word.length() == 0) {
+            System.out.println(word+" ");
+            return;
+        } else {
+            count++;
+            System.out.print(word+" ");
+            generate(word.substring(0, word.length() - 1));
+            generate(word.substring(1, word.length()));
+        }
 
     }
 }
