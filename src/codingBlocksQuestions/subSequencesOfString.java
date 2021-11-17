dec2219cae04f24317c8092df097dbb8ad0773e4
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2021-11-17 14:32:33 +0530

    Trying To Solve The Problem By Prnting The SubSequnces of the String.

---
+++ src/codingBlocksQuestions/subSequencesOfString.java
@@ -0,0 +1,16 @@
+package codingBlocksQuestions;
+
+import java.util.Scanner;
+
+public class subSequencesOfString {
+
+    public static void main(String []args){
+        Scanner sc= new Scanner(System.in);
+        String str = sc.nextLine();
+        String result=printSubsequnces(str);
+    }
+
+    private static String printSubsequnces(String str) {
+
+    }
+}
