ffce5923391d5a4c223eeb28364c450f947e2b8a
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2021-11-22 13:58:09 +0530

    Wrote the Code Succesfully, of the Program.

--- src/codingBlocksQuestions/replacePiWithThreePointOneFour.java
+++ src/codingBlocksQuestions/replacePiWithThreePointOneFour.java
@@ -1,5 +1,7 @@
 package codingBlocksQuestions;
 
+import java.util.Scanner;
+
 /**
  * This is the Qus in which we will get an String as an input and we will
  * Have to replace all the Pi with 3.14.
@@ -7,6 +9,22 @@ package codingBlocksQuestions;
 public class replacePiWithThreePointOneFour {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(replace(str));
+    }
 
+    private static String replace(String str) {
+        String resultantSring = " ";
+        int lenString= str.length();
+        for (int i = 0; i <lenString; i++) {
+            if (i + 1 < lenString && str.charAt(i) == 'p' && str.charAt(i + 1) == 'i') {
+                resultantSring = resultantSring + "3.14";
+                i++;
+            }else{
+                resultantSring=resultantSring+str.charAt(i);
+            }
+        }
+        return resultantSring;
     }
 }
