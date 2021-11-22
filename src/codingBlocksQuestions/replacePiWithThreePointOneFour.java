0a42c1c5b373a4ed651522eee886984a2d73467b
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2021-11-22 13:06:32 +0530

    This is another qus in which we have to replace Pi with 3.14.

---
+++ src/codingBlocksQuestions/replacePiWithThreePointOneFour.java
@@ -0,0 +1,12 @@
+package codingBlocksQuestions;
+
+/**
+ * This is the Qus in which we will get an String as an input and we will
+ * Have to replace all the Pi with 3.14.
+ */
+public class replacePiWithThreePointOneFour {
+
+    public static void main(String[] args) {
+
+    }
+}
