2aab479200866887cdf7c177252e542a45b4030c
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2021-11-22 22:20:17 +0530

    In this class,we will be writing The code of Solving The Problem of Tower Of Hanoi, using Recursive Approach.

---
+++ src/codingBlocksQuestions/towerOfHanoiProblem.java
@@ -0,0 +1,18 @@
+package codingBlocksQuestions;
+
+/**
+ * Google Tower of Hanoi.
+ *
+ * Using a helper stick (peg), shift all rings from peg A to peg B using peg C. All rings are initally placed in ascending order, smallest being on top.
+ *
+ * No bigger ring can be placed over a smaller ring.
+ *
+ * a. Write a recursive function which prints the steps required to solve the tower of Hanoi problem for N discs.
+ *
+ * b. Write a recursive function which returns number of steps required to solve the tower of Hanoi problem for N discs.
+ *
+ * Let T1 be the Source Tower, T2 be the Destination Tower and T3 be the Auxillary Tower.
+ */
+
+public class towerOfHanoiProblem {
+}
