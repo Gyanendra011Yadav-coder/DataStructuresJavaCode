faf9fa026e33e0e4493e6d40a653f4895588c822
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2021-11-29 14:22:59 +0530

    Created this class to write The Code of Tree.

---
+++ src/AdvanceDSAClassCode/BinaryTree.java
@@ -0,0 +1,13 @@
+package AdvanceDSAClassCode;
+
+public class BinaryTree {
+    TreeNode root;
+    private class TreeNode{
+        private TreeNode leftChild;
+        private TreeNode rightChild;
+        private int data;
+        TreeNode(int data){
+            
+        }
+    }
+}
--- src/codingBlocksQuestions/towerOfHanoiProblem.java
+++ src/codingBlocksQuestions/towerOfHanoiProblem.java
@@ -25,7 +25,9 @@ public class towerOfHanoiProblem {
         char source ='1';
         char helper ='2';
         char destination='3';
-        towerOfHanoi(numberOfRings,source,helper,destination);
+        towerOfHanoi2(numberOfRings,source,helper,destination);
+        int res = (int)Math.pow(2,numberOfRings);
+        System.out.println(res-1);
     }
 
     private static void towerOfHanoi(int numberOfDisks, char source, char helper, char destination){
@@ -36,5 +38,29 @@ public class towerOfHanoiProblem {
         towerOfHanoi(numberOfDisks-1,source,destination,helper);
         System.out.println("Move"+" "+numberOfDisks+"th disk from"+" T"+source+" to"+" T"+destination);
         towerOfHanoi(numberOfDisks-1,helper,source,destination);
+        System.out.println();
+    }
+
+    /**
+     * Second Test-Case
+     *
+     * Sample Input
+     * 2
+     * Sample Output
+     * Move 1th disc from T1 to T3
+     * Move 2th disc from T1 to T2
+     * Move 1th disc from T3 to T2
+     * 3
+     */
+
+    private static void towerOfHanoi2(int numberOfDisks, char source, char helper, char destination){
+        if (numberOfDisks==1){
+            System.out.println("Move"+" "+numberOfDisks+"th disk from"+" T"+source+" to"+" T"+destination);
+            return;
+        }
+        towerOfHanoi2(numberOfDisks-1,source,helper,destination);
+        System.out.println("Move"+" "+numberOfDisks+"th disk from"+" T"+source+" to"+" T"+helper);
+        towerOfHanoi2(numberOfDisks-1,destination,source,helper);
+        System.out.println(numberOfDisks+1);
     }
 }
