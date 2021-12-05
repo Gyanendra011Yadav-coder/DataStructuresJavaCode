8da8b0df38a90e29f11a55ec8d1c1e50659b0f4f
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2021-12-05 18:27:36 +0530

    Done Some Correction.

--- src/DynamicProgramming/LongestCommonSubsequne.java
+++ src/DynamicProgramming/LongestCommonSubsequne.java
@@ -1,7 +1,5 @@
 package DynamicProgramming;
-
 import java.util.Scanner;
-
 public class LongestCommonSubsequne {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -11,15 +9,14 @@ public class LongestCommonSubsequne {
         int m=str2.length();
         System.out.println(LongestCommonSubsequne(str1,str2,n,m));
     }
-
     public static int LongestCommonSubsequne(String str1, String str2, int len1,int len2){
         //Base Case Can be That if we See Any length Become 0 then we will return
         if(len1==0 || len2==0 ){
             return 0;
         }
-        if(str1.charAt(len1-1)==str2.charAt(len2)){
-            return 1+LongestCommonSubsequne(str1,str2,len1+1,len2+2);
+        if(str1.charAt(len1-1)==str2.charAt(len2-1)){
+            return 1+LongestCommonSubsequne(str1,str2,len1-1,len2-1);
         }
-        return Math.max(LongestCommonSubsequne(str1, str2, len1-1, len2),LongestCommonSubsequne(str1, str2, len1, len2-1));
+        return Math.max(LongestCommonSubsequne(str1, str2, len1, len2-1),LongestCommonSubsequne(str1, str2, len1-1, len2));
     }
 }
