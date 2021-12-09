2ba0fe6036c0c38062eec4ecda7784af2526f596
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2021-12-09 15:26:43 +0530

    Wrote the Code to take the Input of the User.

--- src/SemisterFifthLabAssignment/activitySelectionProblem.java
+++ src/SemisterFifthLabAssignment/activitySelectionProblem.java
@@ -19,6 +19,20 @@ public class activitySelectionProblem {
         int []activityName=new int[length];
         int []startTime=new int[length];
         int []finishTime= new int[length];
+        System.out.println("Enter The Name Of The Activities:-");
+        System.out.println("Ativities Name Should Be Different with each other\n"+
+                "Try Not Forget To Specify 1,2.... After The Activity Name.");
+        for (int i = 0; i <length; i++) {
+            activityName[i]=scannerObject.nextInt();
+        }
+        System.out.println("Now,Enter The Starting Time Of The Activities:-");
+        for (int j = 0; j <length ; j++) {
+            startTime[j]= scannerObject.nextInt();
+        }
+        System.out.println("Enter The Finishing Time Of The activities:-");
+        for (int k = 0; k <length ; k++) {
+            finishTime[k]=scannerObject.nextInt();
+        }
         activitySelect(activityName,startTime,finishTime);
         System.out.println("Name:"+"Gyanendra Yadav");
         System.out.println("Roll Number:"+"191500309(61)");
