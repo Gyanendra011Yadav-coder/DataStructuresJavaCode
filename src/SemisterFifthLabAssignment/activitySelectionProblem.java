209e588200118a993ab72208e82e10ea0f02afb1
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2021-12-09 15:21:18 +0530

    Implememted the main class as created the arrays to store the array to prform the Activity Selction a/c to Starting time and Finishing Time.

--- src/SemisterFifthLabAssignment/activitySelectionProblem.java
+++ src/SemisterFifthLabAssignment/activitySelectionProblem.java
@@ -1,9 +1,26 @@
 package SemisterFifthLabAssignment;
 
+import java.util.Scanner;
+
 /**
  * @author Gyanendra Yadav
  * @created 09/12/2021- 1:51 PM
  * @project DataStructuresJavaCode
  */
 public class activitySelectionProblem {
+
+
+    public static void activitySelect(int []activityName, int[]startTime, int []finishTime){
+
+    }
+    public static void main(String[] args) {
+        Scanner scannerObject = new Scanner(System.in);
+        int length =scannerObject.nextInt();
+        int []activityName=new int[length];
+        int []startTime=new int[length];
+        int []finishTime= new int[length];
+        activitySelect(activityName,startTime,finishTime);
+        System.out.println("Name:"+"Gyanendra Yadav");
+        System.out.println("Roll Number:"+"191500309(61)");
+    }
 }
