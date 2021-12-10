ede1bc1c5069ce5f9ca8e85f9c4e6e8c72be2735
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2021-12-10 17:41:01 +0530

    Added more code to write down the Algorithms.

---
+++ src/SemisterFifthLabAssignment/binarySerach.java
@@ -0,0 +1,33 @@
+package SemisterFifthLabAssignment;
+
+/**
+ * @author lenovo
+ * @created 10/12/2021- 5:06 PM
+ * @project DataStructuresJavaCode
+ */
+class binarySearch {
+    int binarySearch(int arr[], int left, int right, int key) {
+        if (right >= left) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == key)
+                return mid;
+
+            if (arr[mid] > key)
+                return binarySearch(arr, left, mid - 1, key);
+            return binarySearch(arr, mid + 1, right, key);
+        }
+        return -1;
+    }
+    public static void main(String args[]) {
+        binarySearch ob = new binarySearch();
+        int arr[] = {2, 3, 4, 10, 40};
+        int n = arr.length;
+        int serachingKey = 10;
+        int result = ob.binarySearch(arr, 0, n - 1, serachingKey);
+        if (result == -1)
+            System.out.println("Element not present");
+        else
+            System.out.println("Element found at index "
+                    + result);
+    }
+}
