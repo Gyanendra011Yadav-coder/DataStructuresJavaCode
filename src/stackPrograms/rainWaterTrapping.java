4ec1184fb7034033f96a687ec626094eb8d2ae03
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2022-01-07 00:44:58 +0530

    Wrote the Code to find the maximumn heights of  the buildings from the right and also to find water.

--- src/stackPrograms/rainWaterTrapping.java
+++ src/stackPrograms/rainWaterTrapping.java
@@ -7,7 +7,7 @@ package stackPrograms;
  */
 public class rainWaterTrapping {
     public static void main(String[] args) {
-        int[]heightOfBuildings={0,1,0,2,1,0,1,3,2,1,2,1};
+        int[]heightOfBuildings={4,2,0,3,2,5};
         System.out.println(findRainWaterTrapping(heightOfBuildings));
     }
 
@@ -24,10 +24,16 @@ public class rainWaterTrapping {
         for (int i = 1; i<numberOfBuildings ; i++) {
             leftBuildings[i]=Math.max(leftBuildings[i-1],heightOfBuildings[i]);
         }
-
-
-
-
-
+//Now, we will count the maximum number of Heights of the Buildings at the right side
+        rightBuildings[numberOfBuildings-1]=heightOfBuildings[numberOfBuildings-1];
+        for (int i=numberOfBuildings-2; i>=0 ; i--) {
+            rightBuildings[i]=Math.max(rightBuildings[i+1],heightOfBuildings[i]);
+        }
+//Now,we will count the number of water traps
+        int countWaterStore=0;
+        for (int i = 0; i<heightOfBuildings.length; i++) {
+            countWaterStore+=(Math.min(leftBuildings[i],rightBuildings[i])-heightOfBuildings[i]);
+        }
+    return countWaterStore;
     }
 }
