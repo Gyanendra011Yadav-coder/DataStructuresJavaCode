dc9c0352d32970c699000f5cc2290ae7cf58e0e7
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2022-01-07 00:14:00 +0530

    Created the main method of the class and also have created the method to find out rainWaterTrap.

--- src/stackPrograms/rainWaterTrapping.java
+++ src/stackPrograms/rainWaterTrapping.java
@@ -6,5 +6,15 @@ package stackPrograms;
  * @project DataStructuresJavaCode
  */
 public class rainWaterTrapping {
+    public static void main(String[] args) {
+        int[]heightOfBuildings={0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(findRainWaterTrapping(heightOfBuildings));
+    }
 
+    private static int findRainWaterTrapping(int[] heightOfBuildings) {
+
+        int numberOfBuildings=heightOfBuildings.length;
+
+
+    }
 }
