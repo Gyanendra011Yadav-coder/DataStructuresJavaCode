eadb978f94df25ca3217e1ba3eb8da1c155d9dcf
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2022-01-06 15:45:12 +0530

    Wrote down The Code of Kadane's Algo.

--- src/SevenDaysStrategySheetSolution/questionNumberOneKadanesAlgorithm.java
+++ src/SevenDaysStrategySheetSolution/questionNumberOneKadanesAlgorithm.java
@@ -12,7 +12,19 @@ public class questionNumberOneKadanesAlgorithm {
     }
 
     private static int kadanesAlgorithm(int[] array) {
-        return 0;
+        int overallMax=0;
+        int currentMax=0;
+        for (int i = 0; i < array.length ; i++) {
+            if(currentMax>=0){
+                currentMax=currentMax+array[i];
+            }else{
+                currentMax=array[i];
+            }
+            if(currentMax>overallMax){
+                overallMax=currentMax;
+            }
+        }
+        return overallMax;
     }
 
 }
