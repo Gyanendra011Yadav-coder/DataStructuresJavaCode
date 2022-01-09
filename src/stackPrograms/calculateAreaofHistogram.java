67730c65d611a3f115aceadeba35b1dbae99301b
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2022-01-09 14:16:09 +0530

    Created the main method of the class and also have wrote down the method that will found the Area of the histogram.

--- src/stackPrograms/calculateAreaofHistogram.java
+++ src/stackPrograms/calculateAreaofHistogram.java
@@ -1,9 +1,51 @@
 package stackPrograms;
 
+import java.util.Stack;
+
 /**
  * @author Gyanendra Yadav
  * @created 09/01/2022- 2:02 PM
  * @project DataStructuresJavaCode
  */
 public class calculateAreaofHistogram {
+    public static void main(String[] args) {
+        int []heights={};
+
+    }
+
+    private static int calculateArea(int[]heights){
+        int maxArea=0;
+        int []previousSmallerValues=calculatePreviousSmall(heights);
+        int []nextSmallerValues=calculateNextSmall(heights);
+        for (int i = 0; i <heights.length ; i++) {
+            int current=(previousSmallerValues[i]-nextSmallerValues[i]-1)*heights[i];
+            maxArea=Math.max(current,maxArea);
+        }
+        return maxArea;
+    }
+
+
+
+    private static int[] calculatePreviousSmall(int []array){
+        int [] arrayPrevious=new int[array.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i <array.length ; i++) {
+            while (!stack.isEmpty() && array[stack.peek()]>=array[i]){
+                stack.pop();
+            }
+            if(stack.isEmpty()==true){
+                arrayPrevious[i]=-1;
+            }else{
+                arrayPrevious[i]=stack.peek();
+            }
+            stack.push(i);
+        }
+        return arrayPrevious;
+    }
+    private static int[] calculateNextSmall(int[] array) {
+        int [] arrayNext=new int[array.length];
+
+
+        return arrayNext;
+    }
 }
