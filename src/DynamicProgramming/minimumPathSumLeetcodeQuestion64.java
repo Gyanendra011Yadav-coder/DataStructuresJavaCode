8855d2b2391b5b6ebd736d08b08d505bad602a79
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2022-01-18 01:25:32 +0530

    Added the code of dynamic programming.

--- src/DynamicProgramming/minimumPathSumLeetcodeQuestion64.java
+++ src/DynamicProgramming/minimumPathSumLeetcodeQuestion64.java
@@ -1,5 +1,7 @@
 package DynamicProgramming;
 
+import java.util.Arrays;
+
 /**
  * @author Gyanendra Yadav
  * @created 18/01/2022- 12:32 AM
@@ -8,10 +10,15 @@ package DynamicProgramming;
 public class minimumPathSumLeetcodeQuestion64 {
     public static void main(String[] args) {
         int [][]gridArray={{ 1, 3, 1 }, { 1, 5, 1 }, { 4, 2, 1 }};
-        System.out.println(findMinimumPathSum(gridArray,0,0));
+
+        int [][]dp= new int[gridArray.length][gridArray[0].length];
+        for(int []array:dp){
+            Arrays.fill(array,-1);
+        }
+        System.out.println(findMinimumPathSum(gridArray,0,0,dp));
     }
 
-    private static int findMinimumPathSum(int[][] grid, int cr, int cc) {
+    private static int findMinimumPathSum(int[][] grid, int cr, int cc,int [][]dp) {
 
         if (cc >= grid[0].length || cr >= grid.length) {
             return Integer.MAX_VALUE;
@@ -20,9 +27,13 @@ public class minimumPathSumLeetcodeQuestion64 {
             return grid[cr][cc];
 
         }
+        //applying dp
+        if(dp[cr][cc]!=-1){
+            return grid[cr][cc];
+        }
 
-        int right = findMinimumPathSum(grid, cr, cc + 1);
-        int down = findMinimumPathSum(grid, cr + 1, cc);
-        return  (Math.min(right, down) + grid[cr][cc]);
+        int right = findMinimumPathSum(grid, cr, cc + 1,dp);
+        int down = findMinimumPathSum(grid, cr + 1, cc,dp);
+        return  dp[cr][cc]=Math.min(right, down) + grid[cr][cc];
     }
 }
