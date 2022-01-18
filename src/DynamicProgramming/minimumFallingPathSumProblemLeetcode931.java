1bfdf146b8c69d5327b972ba2bd64c1a7702188c
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2022-01-18 23:48:17 +0530

    created the 2d ARRAY.

--- src/DynamicProgramming/minimumFallingPathSumProblemLeetcode931.java
+++ src/DynamicProgramming/minimumFallingPathSumProblemLeetcode931.java
@@ -5,5 +5,17 @@ package DynamicProgramming;
  * @created 18/01/2022- 11:45 PM
  * @project DataStructuresJavaCode
  */
+
+/**
+ * Input: matrix = [[2,1,3],[6,5,4],[7,8,9]]
+ * Output: 13
+ * Explanation: There are two falling paths with a minimum sum as shown.
+ */
 public class minimumFallingPathSumProblemLeetcode931 {
+    public static void main(String[] args) {
+        int [][]grid= {{2,1,3},{6,5,4},{7,8,9}};
+
+    }
+
+
 }
