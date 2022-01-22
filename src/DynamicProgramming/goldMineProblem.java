1f65b1f36216981b928867a1256f41434bdc93b7
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2022-01-22 14:59:44 +0530

    Created the main class of the class.

--- src/DynamicProgramming/goldMineProblem.java
+++ src/DynamicProgramming/goldMineProblem.java
@@ -1,9 +1,25 @@
 package DynamicProgramming;
 
+import java.util.Scanner;
+
 /**
  * @author Gyanendra Yadav
  * @created 22/01/2022- 2:54 PM
  * @project DataStructuresJavaCode
  */
 public class goldMineProblem {
+    public static void main(String[] args) {
+        Scanner scannerObject =new Scanner(System.in);
+        int numberRows=scannerObject.nextInt();
+        int numberColumns=scannerObject.nextInt();
+        int [][]mine=new int [numberRows][numberColumns];
+        for (int i = 0; i <numberRows ; i++) {
+            for (int j = 0; j < numberColumns; j++) {
+                mine[i][j]=scannerObject.nextInt();
+            }
+        }
+
+
+    }
+
 }
