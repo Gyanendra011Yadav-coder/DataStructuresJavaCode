3818f7290d59597a4f8fd92445590b44e0d2d884
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2022-02-03 17:53:30 +0530

    Added some more codes in the classes.

--- src/Arrays/chocalateDistribution.java
+++ src/Arrays/chocalateDistribution.java
@@ -6,5 +6,24 @@ package Arrays;
  * @project DataStructuresJavaCode
  */
 public class chocalateDistribution {
+    public static void main(String[] args) {
 
+    }
+
+    private static int countMinChocalteLeft(int []chocalte,int numberOfChocaltesPack, int numberOfBoys){
+        if(numberOfBoys==0 || numberOfChocaltesPack==0 ){
+            return 0;
+        }
+        if(numberOfBoys==numberOfChocaltesPack){
+            return chocalte[numberOfChocaltesPack-1]-chocalte[0];
+        }
+        if(numberOfBoys>numberOfChocaltesPack){
+            return -1;
+        }
+        int min_Diff=Integer.MAX_VALUE;
+        for (int i = 0; i <numberOfChocaltesPack ; i++) {
+            min_Diff=Math.min((chocalte(i+numberOfBoys-1)-chocalte(i)),min_Diff);
+        }
+        return min_Diff;
+    }
 }
---
+++ src/demo/hackfun1.java
@@ -0,0 +1,18 @@
+package demo;
+
+/**
+ * @author lenovo
+ * @created 02/02/2022- 9:39 PM
+ * @project DataStructuresJavaCode
+ */
+public class hackfun1 {
+    private String hack_str;
+
+    public hackfun1() {
+        hack_str = "HackerEarth";
+    }
+
+    private void hack_method() {
+        System.out.println("Hello HackerEsarth!");
+    }
+}
