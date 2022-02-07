63eea5c041b5c87e67a4c97b829a75b6a5cf8d02
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2022-02-07 15:39:52 +0530

    Added the code in the method and wrote the code for it's base condition.

--- src/Hashing/hashMap/findDistinctElementFromKSizeWindow.java
+++ src/Hashing/hashMap/findDistinctElementFromKSizeWindow.java
@@ -1,9 +1,23 @@
 package Hashing.hashMap;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Gyanendra Yadav
  * @created 07/02/2022- 3:34 PM
  * @project DataStructuresJavaCode
  */
 public class findDistinctElementFromKSizeWindow {
+    public static void main(String[] args) {
+
+    }
+
+    private static void findKDistinctElement(int []array, int k){
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<k ;i++){
+            //this is checking if the defined number is present then it will increase it's count else we will add it and increase it's count
+            map.put(array[i],map.getOrDefault(array[i],0)+1);
+        }
+    }
 }
