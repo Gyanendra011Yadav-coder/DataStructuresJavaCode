adbec64b14df7048563afba14692d4ee8b0670ea
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2022-02-08 19:52:57 +0530

    wrote this method that wil be solving another problem of based on prioroty queue.

---
+++ src/priorityQueue/findMedianOfRunningStream.java
@@ -0,0 +1,26 @@
+package priorityQueue;
+
+/**
+ * @author Gyanendra Yadav
+ * @created 08/02/2022- 7:51 PM
+ * @project DataStructuresJavaCode
+ */
+public class findMedianOfRunningStream {
+    //Function to insert heap.
+    public static void insertHeap(int x)
+    {
+        // add your code here
+    }
+
+    //Function to balance heaps.
+    public static void balanceHeaps()
+    {
+        // add your code here
+    }
+
+    //Function to return Median.
+    public static double getMedian()
+    {
+        // add your code here
+    }
+}
