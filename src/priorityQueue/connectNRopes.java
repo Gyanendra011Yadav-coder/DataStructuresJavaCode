256cfcf9d8546b2603c306d61276679d5c2c0366
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2022-02-08 18:13:45 +0530

    Created this class to solve another question that find smalles cost as possible for connecting the ropes of diffrent length.

---
+++ src/priorityQueue/connectNRopes.java
@@ -0,0 +1,17 @@
+package priorityQueue;
+
+/**
+ * @author lenovo
+ * @created 08/02/2022- 6:08 PM
+ * @project DataStructuresJavaCode
+ */
+public class connectNRopes {
+    //Function to return the minimum cost of connecting the ropes.
+    long minCost(long arr[], int n)
+    {
+        long cost=0;
+        // your code here
+
+        return cost;
+    }
+}
