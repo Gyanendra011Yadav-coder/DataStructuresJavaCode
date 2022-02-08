778313404066b38623018ed4554ce97df93976a2
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2022-02-08 18:34:27 +0530

    worte the function that write solve the code tha count minimum cost for conencting rope.

--- src/priorityQueue/connectNRopes.java
+++ src/priorityQueue/connectNRopes.java
@@ -1,7 +1,9 @@
 package priorityQueue;
 
+import java.util.PriorityQueue;
+
 /**
- * @author lenovo
+ * @author Gyanendra Yadav
  * @created 08/02/2022- 6:08 PM
  * @project DataStructuresJavaCode
  */
@@ -9,9 +11,19 @@ public class connectNRopes {
     //Function to return the minimum cost of connecting the ropes.
     long minCost(long arr[], int n)
     {
+        PriorityQueue<Long> pq = new PriorityQueue<>();
         long cost=0;
         // your code here
-
+        for(int i=0; i<arr.length ; i++){
+            pq.add(arr[i]);
+        }
+        while(pq.size()!=1){
+            long firstNumber= pq.poll();
+            long secondNumber=pq.poll();
+            long sum=firstNumber+secondNumber;
+            cost+=sum;
+            pq.add(sum);
+        }
         return cost;
     }
 }
