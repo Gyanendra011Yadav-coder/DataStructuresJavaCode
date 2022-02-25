6b1df93d7ffb9006c7cd1d525652ce46970d5a55
Gyanendra Yadav <gyanendra1018yadav@gmail.com>
2022-02-25 19:22:09 +0530

    wrote the code to solve the problem have created methods that will find 4 sum and 3 sum of the given array according to the target value that user have provided.

--- src/setAndMapQuestions/fourSumLeetcodeQuestionEighteen.java
+++ src/setAndMapQuestions/fourSumLeetcodeQuestionEighteen.java
@@ -1,4 +1,43 @@
 package setAndMapQuestions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class fourSumLeetcodeQuestionEighteen {
+
+    private static List<List<Integer>> fourSum(int[] num, int target) {
+        List<List<Integer>> resultList = new ArrayList<>();
+        if (num.length < 4) {
+            return resultList;
+        }
+        Arrays.sort(num);
+        for (int i = 0; i < num.length; i++) {
+            if (i != 0 && num[i] == num[i - 1]) {
+                continue;
+            }
+            int val1 = num[i];
+            List<List<Integer>> subResult = threeSum(num, target - val1, i);
+            for (List<Integer> list : subResult) {
+                list.add(val1);
+                resultList.add(list);
+            }
+        }
+        return resultList;
+    }
+
+    private static List<List<Integer>> threeSum(int[] num, int target, int startingIndex) {
+        List<List<Integer>> resultList= new ArrayList<>();
+        if(num.length<3){
+            return resultList;
+        }
+        for(int i=0; i<=num.length-3;i++){
+            List<List<Integer>> subResult= new ArrayList<>();
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
