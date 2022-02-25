89a4d87cffbcb4de2b79b5f611df5bebd110fe76
Gyanendra Yadav <gyanendra1018yadav@gmail.com>
2022-02-25 20:58:54 +0530

    added the piece of code in two pointer method .

--- src/setAndMapQuestions/fourSumLeetcodeQuestionEighteen.java
+++ src/setAndMapQuestions/fourSumLeetcodeQuestionEighteen.java
@@ -1,5 +1,6 @@
 package setAndMapQuestions;
 
+import java.net.Inet4Address;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -28,12 +29,15 @@ public class fourSumLeetcodeQuestionEighteen {
 
     private static List<List<Integer>> threeSum(int[] num, int target, int startingIndex) {
         List<List<Integer>> resultList= new ArrayList<>();
-        if(num.length<3){
+        if(num.length-startingIndex<3){
             return resultList;
         }
         for(int i=startingIndex; i<=num.length-3;i++){
+            if(i!=startingIndex && num[i]==num[i-1]){
+                continue;
+            }
             int val1=num[i];
-            List<List<Integer>> subResult= twoSum(num,target-val1,i+1);
+            List<List<Integer>> subResult= twoSum(num,i+1,num.length-1,target-val1);
             for(List <Integer> list:subResult){
                 list.add(val1);
                 resultList.add(list);
@@ -42,7 +46,32 @@ public class fourSumLeetcodeQuestionEighteen {
         return resultList;
     }
 
-    private static List<List<Integer>> twoSum(int[] num, int i, int i1) {
+    private static List<List<Integer>> twoSum(int[] num, int startingIndex, int endingIndex, int target ) {
+        int leftIndex=startingIndex,rightIndex=endingIndex;
+        List <List<Integer>> resultList= new ArrayList<>();
+        if(num.length<2){
+            return resultList;
+        }
+        while(leftIndex<rightIndex){
+            if(leftIndex!=startingIndex && num[leftIndex]==num[leftIndex-1]){
+                leftIndex++;
+                continue;
+            }
+            int sum=num[leftIndex]+num[rightIndex];
+            if(sum<target){
+                List<Integer> subResult = new ArrayList<>();
+                subResult.add(num[ leftIndex]);
+                subResult.add(num[rightIndex]);
+                resultList.add(subResult);
+                leftIndex++;
+                rightIndex--;
+            }else if(sum>target){
+                rightIndex--;
+            }else{
+                leftIndex++;
+            }
+        }
+        return resultList;
     }
 
     public static void main(String[] args) {
