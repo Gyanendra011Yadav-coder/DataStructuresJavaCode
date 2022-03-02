83d7d07f37025268d8252e522aeed2f1d99fada4
Gyanendra Yadav <gyanendra1018yadav@gmail.com>
2022-03-02 12:15:47 +0530

    wrote the whole code to add the new Node in the tree.

--- src/TreeDataStructure/binaryTreeImplementation.java
+++ src/TreeDataStructure/binaryTreeImplementation.java
@@ -1,5 +1,9 @@
 package TreeDataStructure;
 
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.Queue;
+
 public class binaryTreeImplementation {
 
     public class treeNode{
@@ -19,7 +23,29 @@ public class binaryTreeImplementation {
         if(rootNode==null){
             rootNode=newNode;
         }
+        //This Code Will be traversing the tree
+        Queue<treeNode> q = new LinkedList<>();
+        treeNode tempNode=rootNode;
+        q.add(tempNode);
+        while(!q.isEmpty()){
+            tempNode=q.poll();
+            q.remove();
+            if(tempNode.leftChild==null){
+                tempNode.leftChild=new treeNode(data);
+                break;
+            }else{
+                q.add(tempNode.leftChild);
+            }
+            //this coondition will be checking if the right child of the node is null then create new tree Node at that location else add the rightNode of tree in the queue
+            if(tempNode.rightChild==null){
+                tempNode.rightChild=new treeNode(data);
+                break;
+            }else{
+                q.add(tempNode.leftChild);
+            }
+        }
     }
 
 
 }
+
