735034acb91dcca37682e1c67d2165c4cd182f78
Gyanendra Yadav <gyanendra1018yadav@gmail.com>
2022-03-02 12:06:40 +0530

    write the code to traverse the Binary Tree.

--- src/TreeDataStructure/binaryTreeImplementation.java
+++ src/TreeDataStructure/binaryTreeImplementation.java
@@ -1,4 +1,25 @@
 package TreeDataStructure;
 
 public class binaryTreeImplementation {
+
+    public class treeNode{
+        int data;
+        treeNode leftChild;
+        treeNode rightChild;
+        public treeNode(int value){
+            data=value;
+            leftChild=null;
+            rightChild=null;
+        }
+    }
+    treeNode rootNode=null;
+    //Insettion in Binary Tree
+    public void insert(int data){
+        treeNode newNode= new treeNode(data);
+        if(rootNode==null){
+            rootNode=newNode;
+        }
+    }
+
+
 }
