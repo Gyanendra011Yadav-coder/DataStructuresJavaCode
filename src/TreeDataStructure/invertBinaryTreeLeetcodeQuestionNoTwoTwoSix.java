20e0ae3bdb970ae558ff7f2e206a23961f4cf8d7
Gyanendra Yadav <gyanendra1018yadav@gmail.com>
2022-03-04 01:04:42 +0530

    Corrected the code.

---
+++ src/TreeDataStructure/invertBinaryTreeLeetcodeQuestionNoTwoTwoSix.java
@@ -0,0 +1,28 @@
+package TreeDataStructure;
+
+public class invertBinaryTreeLeetcodeQuestionNoTwoTwoSix {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int value) {
+            val = value;
+            left = null;
+            right = null;
+        }
+    }
+
+    binaryTreeImplementation.TreeNode rootNode = null;
+
+    public TreeNode invertTree(TreeNode root) {
+        if(root!=null){
+            TreeNode temp=root.left;
+            temp.left=temp.right;
+            temp.right=temp;
+            invertTree(temp.left);
+            invertTree(temp.right);
+        }
+        return root;
+    }
+}
