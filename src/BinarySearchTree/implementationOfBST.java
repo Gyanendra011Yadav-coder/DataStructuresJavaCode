f18536b66dd57e02780f64b8708a2f666ef0f495
Gyanendra Yadav <gyanendra1018yadav@gmail.com>
2022-03-04 01:35:53 +0530

    Added the method that will insert the new Nodes in the BST.

--- src/BinarySearchTree/implementationOfBST.java
+++ src/BinarySearchTree/implementationOfBST.java
@@ -14,14 +14,28 @@ public class implementationOfBST {
     }
 
     private TreeNode root=null;
-    //Method to insert Data in BST
+
+    /**Method to insert Data in BST*/
     private void insert(int data){
+        root=insertUtil(root,data);
+    }
+
+    private TreeNode insertUtil(TreeNode root, int data) {
         TreeNode newNode= new TreeNode(data);
         if(root==null){
             root=newNode;
+            return root;
         }
-        //Now, we will check if
-
-
+        /**Now, we will check, if the newNode has the value greater than root node or
+         less than the root value.
+         1) if it is greater than root than it will be kept at the right side of root
+         2)else, we will be keep it at left side.
+         */
+        if(data<root.val){
+            insertUtil(root.left,data);
+        }else{
+            insertUtil(root.right,data);
+        }
+        return root;
     }
 }
