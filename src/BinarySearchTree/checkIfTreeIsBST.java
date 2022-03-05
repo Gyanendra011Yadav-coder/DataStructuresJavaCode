4966399a34da1d6b6f18c2de66941e3253874fd5
Gyanendra Yadav <gyanendra1018yadav@gmail.com>
2022-03-05 13:00:14 +0530

    formatted the code

--- src/BinarySearchTree/checkIfTreeIsBST.java
+++ src/BinarySearchTree/checkIfTreeIsBST.java
@@ -21,21 +21,23 @@ public class checkIfTreeIsBST {
     public boolean isValidBST(TreeNode root) {
         return isBSTUtil(root);
     }
-//this method contains the logic to check is the tree is BST or not
-    TreeNode prev=null; // creating  this Node as it will contains the  previous node, so that we cam compare it wih next node values.
+
+    //this method contains the logic to check is the tree is BST or not
+    TreeNode prev = null; // creating  this Node as it will contains the  previous node, so that we cam compare it wih next node values.
+
     private boolean isBSTUtil(TreeNode root) {
         //if node is not null then we will check else we will directly return true is node is null
-        if(root!=null){
-            if(!isBSTUtil(root.left)){
+        if (root != null) {
+            if (!isBSTUtil(root.left)) {
                 return false;
             }
             //if previous node's value is greater than current node that means it is not following sorting. So, returnign false
-             if(prev!=null && prev.val>=root.val){
-                 return false;
-             }
-             //and if prev node value < current node then we will update the prev node with cuurent node
-             prev=root;
-             isBSTUtil(root.right);
+            if (prev != null && prev.val >= root.val) {
+                return false;
+            }
+            //and if prev node value < current node then we will update the prev node with cuurent node
+            prev = root;
+            isBSTUtil(root.right);
         }
         return true;
     }
