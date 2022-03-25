618b3b1e04711488e8c6debe8458aadfb601faea
Gyanendra Yadav <gyanendra1018yadav@gmail.com>
2022-03-25 17:44:00 +0530

    Wrote the code succesfully, by sorting the array using Lambda function and then calculatiing the costs of the interviews to send to diffrent cities to diffrent cities.

--- src/Arrays/twoCitySchedulingLeetcode1029.java
+++ src/Arrays/twoCitySchedulingLeetcode1029.java
@@ -1,7 +1,20 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class twoCitySchedulingLeetcode1029 {
     public int twoCitySchedCost(int[][] costs) {
-
+        int numberOfPersons=costs.length;
+        int prices=0;
+        Arrays.sort(costs,(a,b)->{
+           return (a[0]-a[1])-(b[0]-b[1]);
+        });
+        for (int i = 0; i <numberOfPersons/2 ; i++) {
+            prices+=costs[i][0];
+        }
+        for (int j = numberOfPersons/2; j <numberOfPersons; j++) {
+            prices+=costs[j][1];
+        }
+        return prices;
     }
 }
