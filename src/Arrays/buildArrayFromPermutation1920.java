1ca6be73b8718dd73c2e45d33bb9aaefc72b6742
Gyanendra Yadav <gyanendra1018yadav@gmail.com>
2022-03-28 12:58:09 +0530

    New question solved of leetcode.

---
+++ src/Arrays/buildArrayFromPermutation1920.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class buildArrayFromPermutation1920 {
+    public int[] buildArray(int[] nums) {
+        int len= nums.length;
+        int []arr = new int[len];
+        for(int i=0;i<len;i++){
+            arr[i]=nums[nums[i]];
+        }
+        return arr;
+    }
+}
