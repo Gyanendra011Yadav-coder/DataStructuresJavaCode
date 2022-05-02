48eecc2fedb3f43c3814072fb1f81423e9c977d0
GYANENDRA\Gyanendra <gyanendra.yadav@gaussnetworks.com>
2022-05-02 17:45:33 +0530

    Added the piece of Code to solve the problem.

--- src/LeetcodeQuestions/moveZeroesProblemNumber283.java
+++ src/LeetcodeQuestions/moveZeroesProblemNumber283.java
@@ -6,7 +6,7 @@ import java.util.Queue;
 public class moveZeroesProblemNumber283 {
 
     public void moveZeroes(int[] nums) {
-        int length=nums.length, countZeroes=0;
+        /*int length=nums.length, countZeroes=0;
         Queue <Integer> queue = new PriorityQueue<Integer>();
         for (int i = 0; i <length ; i++) {
             if(nums[i]!=0){
@@ -20,9 +20,23 @@ public class moveZeroesProblemNumber283 {
                 queue.add(0);
             }
         }
-
         for (int i = 0; i <length ; i++) {
+            nums[i]=queue.poll();
+        }*/
 
+        //solution-02
+        int length=nums.length,count=0;
+        //Shifting Non-Numeric values
+        for (int i = 0; i <length ; i++) {
+            if(nums[i]!=0){
+                nums[count]=nums[i];
+                count++;
+            }
         }
+        //Inserting Zeroes
+        for (int i = count; i <length ; i++) {
+            nums[i]=0;
+        }
+
     }
 }
