f4f5aee47da1357f8c23fa3fa04a72885c40438f
GYANENDRA\Gyanendra <gyanendra.yadav@gaussnetworks.com>
2022-05-02 17:20:23 +0530

    Solved second day of month may problem of leetcode.

---
+++ src/LeetcodeQuestions/sortArrayByParity905.java
@@ -0,0 +1,34 @@
+package LeetcodeQuestions;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+
+public class sortArrayByParity905 {
+
+    //method to solve this leetcode problem
+    public int[] sortArrayByParity(int[] nums) {
+        int lengthOfArray=nums.length;
+        Queue <Integer> even = new PriorityQueue<Integer>();
+        Queue<Integer> odd = new PriorityQueue<Integer>();
+        for (int i=0; i<lengthOfArray ; i++) {
+            if(nums[i]%2==0){
+                even.add(nums[i]);
+            }else{
+                odd.add(nums[i]);
+            }
+        }
+        int i=0;
+        while(!even.isEmpty()){
+            nums[i]=even.poll();
+            i++;
+        }
+        while(!odd.isEmpty()){
+            nums[i]=odd.poll();
+            i++;
+        }
+        return nums;
+    }
+    public static void main(String[] args) {
+
+    }
+}
