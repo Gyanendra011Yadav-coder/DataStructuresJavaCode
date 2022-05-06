1e9755f6ecf46e20509854973a40bce982cc3ad7
GYANENDRA\Gyanendra <gyanendra.yadav@gaussnetworks.com>
2022-05-06 14:42:02 +0530

    Solved fourther more questions of leetcode.

---
+++ src/LeetcodeQuestions/ImplementStackusingQueues225.java
@@ -0,0 +1,27 @@
+package LeetcodeQuestions;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ImplementStackusingQueues225 {
+    Queue <Integer> stack;
+    public ImplementStackusingQueues225() {
+        stack = new LinkedList<Integer>();
+    }
+    public void push(int x) {
+        stack.add(x);
+    }
+
+    public int pop() {
+        return stack.remove();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public boolean empty() {
+        return stack.isEmpty();
+    }
+}
+
