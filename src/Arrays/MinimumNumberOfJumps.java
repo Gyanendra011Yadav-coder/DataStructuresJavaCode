cafd258c1d4f2966795098dd66aa3647ebe66972
DESKTOP-FRL0M1V\Gyanendra-Yadav <gyanendra.yadav@deltatech.gg>
2022-09-11 21:52:53 +0530

    adding new code to solve the problem of Arrays in Java.

---
+++ src/Arrays/MinimumNumberOfJumps.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+/**
+ * Created By: GyanendraYadav on 2022-09-11,Sep,2022
+ */
+public class MinimumNumberOfJumps {
+    static int minJumps(int[] arr){
+        int stepsCount=-1;
+        int inde=0;
+
+        return stepsCount;
+    }
+    public static void main(String[] args) {
+
+    }
+}
