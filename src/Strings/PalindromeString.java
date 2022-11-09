d414e965b43f0e03ebb78f023af8205e58a6cc6a
DESKTOP-FRL0M1V\Gyanendra-Yadav <gyanendra.yadav@deltatech.gg>
2022-11-09 11:15:56 +0530

    Solving another problems of DSA: 1) Reverse Squared Sum 2) Is String is Plaindrome

---
+++ src/Strings/PalindromeString.java
@@ -0,0 +1,17 @@
+package Strings;
+
+/**
+ * Created By:  Gyanendra_Yadav
+ * on 2022-11-09,Nov,2022
+ * in Project: DataStructuresJavaCode
+ */
+public class PalindromeString {
+    public void main(String[] args) {
+       String strCheckFunction= "abba";
+        System.out.println(isPalindrome(strCheckFunction));
+    }
+    int isPalindrome(String S) {
+        // code here
+
+    }
+}
