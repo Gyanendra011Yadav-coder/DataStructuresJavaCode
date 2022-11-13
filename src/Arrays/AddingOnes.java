dc6c0d5deefc54b851730d98efbf1d9f9c1a7c04
DESKTOP-FRL0M1V\Gyanendra-Yadav <gyanendra.yadav@deltatech.gg>
2022-11-13 15:05:43 +0530

    Solving another problem of Array in this we need to create an array from the given array and then we will have to add last index value to the next element.

---
+++ src/Arrays/AddingOnes.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+/**
+ * Created By:  Gyanendra_Yadav
+ * on 2022-11-13,Nov,2022
+ * in Project: DataStructuresJavaCode
+ *
+ * ****************************************************************
+ * Input:
+ * 3 4
+ * 1 1 2 3
+ * Output:
+ * 2 3 4
+ * Explanation:
+ * Initially the array is {0, 0, 0}. After the
+ * first 1, it becomes {1, 1, 1}. After the
+ * second 1 it becomes {2, 2, 2}. After 2, it
+ * becomes {2, 3, 3}. After 3 it becomes, {2, 3, 4}.
+ * ****************************************************************
+ *
+ */
+public class AddingOnes {
+    public static void main(String[] args) {
+
+    }
+}
