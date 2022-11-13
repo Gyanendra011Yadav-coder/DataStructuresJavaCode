218e37e6e2cdfbe0866449ffefdca21953a33d1f
DESKTOP-FRL0M1V\Gyanendra-Yadav <gyanendra.yadav@deltatech.gg>
2022-11-13 15:03:10 +0530

    Solving more problems of Data Structure and algorithms

---
+++ src/Arrays/PrefixSumArray.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+/**
+ * Created By:  Gyanendra_Yadav
+ * on 2022-11-13,Nov,2022
+ * in Project: DataStructuresJavaCode
+ ****************************************************************************
+ * Example:
+ * Input  : arr[] = {10, 20, 10, 5, 15}
+ * Output : prefixSum[] = {10, 30, 40, 45, 60}
+ * Explanation : While traversing the array, update the element by adding it with its previous element.
+ * prefixSum[0] = 10,
+ * prefixSum[1] = prefixSum[0] + arr[1] = 30,
+ * prefixSum[2] = prefixSum[1] + arr[2] = 40 and so on.
+ ******************************************************************************
+ */
+public class PrefixSumArray {
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 10, 5, 15};
+
+    }
+
+    //Method to UpDate Data in the Array
+    public static void update(int a[], int n, int updates[], int k)
+    {
+        // Your code goes here
+    }
+}
