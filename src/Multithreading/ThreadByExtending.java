78c5166abadf7257adf381e8b2247e677f97a0ec
DESKTOP-FRL0M1V\Gyanendra-Yadav <gyanendra.yadav@deltatech.gg>
2023-01-10 15:21:59 +0530

    Creating new class to understand Multi-Threading in java.

---
+++ src/Multithreading/ThreadByExtending.java
@@ -0,0 +1,10 @@
+package Multithreading;
+
+/**
+ * Created By:  Gyanendra_Yadav
+ * on 2023-01-10,Jan,2023
+ * in Project: DataStructuresJavaCode
+ */
+public class ThreadByExtending {
+
+}
---
+++ src/Multithreading/ThreadByRunnableInterface.java
@@ -0,0 +1,9 @@
+package Multithreading;
+
+/**
+ * Created By:  Gyanendra_Yadav
+ * on 2023-01-10,Jan,2023
+ * in Project: DataStructuresJavaCode
+ */
+public class ThreadByRunnableInterface {
+}
