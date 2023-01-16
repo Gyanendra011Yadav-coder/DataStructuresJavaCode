0edf2b524e2f0625c8827deee8593860707b55b7
DESKTOP-FRL0M1V\Gyanendra-Yadav <gyanendra.yadav@deltatech.gg>
2023-01-16 12:11:47 +0530

    Creating new class to understand Multi-Threading in java. and solving one more questions of Arrays.

---
+++ src/Arrays/NextGreaterElement.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+/**
+ * Created By:  Gyanendra_Yadav
+ * on 2023-01-16,Jan,2023
+ * in Project: DataStructuresJavaCode
+ *
+ *
+ * Input:
+ * n = 4, arr[] = [1 3 2 4]
+ * Output:
+ * 3 4 4 -1
+ * Explanation:
+ * In the array, the next larger element
+ * to 1 is 3, 3 is 4, 2 is 4 and for 4?
+ * since it doesn't exist, it is -1.
+ *
+ */
+public class NextGreaterElement {
+}
--- src/Multithreading/ThreadByRunnableInterface.java
+++ src/Multithreading/ThreadByRunnableInterface.java
@@ -1,9 +1,33 @@
 package Multithreading;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 /**
  * Created By:  Gyanendra_Yadav
  * on 2023-01-10,Jan,2023
  * in Project: DataStructuresJavaCode
  */
-public class ThreadByRunnableInterface {
+public class ThreadByRunnableInterface implements Runnable{
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("MyThread: " + i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ThreadByRunnableInterface myThread = new ThreadByRunnableInterface();
+        Thread threadObj= new Thread(myThread);
+        threadObj.start();
+    }
+
+
 }
