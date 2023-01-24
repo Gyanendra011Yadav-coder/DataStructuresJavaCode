55dfed024747fb0986b713564b6bf4198360ae29
DESKTOP-FRL0M1V\Gyanendra-Yadav <gyanendra.yadav@deltatech.gg>
2023-01-24 10:58:57 +0530

    adding comments.

--- src/Arrays/ConvertArrayReducedForm.java
+++ src/Arrays/ConvertArrayReducedForm.java
@@ -22,6 +22,17 @@ public class ConvertArrayReducedForm {
     //Method to convert
     static void convert(int[] arr, int n) {
         //Making the duplicate copy the code
+
+        /*
+        In the code provided, the line "int[] duplicateArray = arr;" creates a reference to the same array in memory as the original array "arr". It does not create a new copy of the array. Therefore, when the Arrays.sort() method is called on "duplicateArray", it sorts the same array in memory that "arr" references.
+
+This means that the original array "arr" is also getting sorted and the duplicateArray is also pointing to the same location in the memory. So, the duplicateArray and arr both will be sorted.
+
+If you want to sort the duplicateArray separately, you should use the Arrays.copyOf() function to create a new copy of the array.
+For example, int[] duplicateArray = Arrays.copyOf(arr, arr.length);
+
+This will create a new copy of the original array, so sorting the duplicateArray will not affect the original array.
+         */
         int[] duplicateArray =  Arrays.copyOf(arr, arr.length);;
 
         //Sorting the array
