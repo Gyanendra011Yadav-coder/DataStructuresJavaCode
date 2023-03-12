e8de6e2efbed5980c6824be8b0774fe83b2bccd5
DESKTOP-FRL0M1V\Gyanendra-Yadav <gyanendra.yadav@deltatech.gg>
2023-03-12 18:25:47 +0530

    Start Solving prepBytes Questions of Searching.

---
+++ src/pepCodingQuestions/Searching/SmallestNumber.java
@@ -0,0 +1,32 @@
+package pepCodingQuestions.Searching;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Created By:  Gyanendra_Yadav
+ * on 2023-03-12,Mar,2023
+ * in Project: DataStructuresJavaCode
+ *
+ * Input:
+ * First-line contains
+ * N
+ * ,
+ * The second line contains
+ * N
+ *  space-separated integers.
+ * The third line contains
+ * K
+ * .
+ *
+ * Output
+ * Print the smallest number in the array which occurs exactly
+ * K
+ *  times in an array.
+ */
+public class SmallestNumber {
+    public static void main(String args[]) throws IOException {
+        Scanner scanner=new Scanner(System.in);
+
+    }
+}
