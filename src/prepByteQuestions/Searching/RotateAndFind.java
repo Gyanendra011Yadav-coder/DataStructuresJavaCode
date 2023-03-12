1c23c82757c481919fd8e65a94999f8f8bf0024d
DESKTOP-FRL0M1V\Gyanendra-Yadav <gyanendra.yadav@deltatech.gg>
2023-03-12 20:13:00 +0530

    Solving another problem of searching.

---
+++ src/prepByteQuestions/Searching/RotateAndFind.java
@@ -0,0 +1,29 @@
+package prepByteQuestions.Searching;
+
+/**
+ * Created By:  Gyanendra_Yadav
+ * on 2023-03-12,Mar,2023
+ * in Project: DataStructuresJavaCode
+ *
+ * ------------------------------------------------------
+ * Given a rotated array A� and an integer x�, your task is to find the index of x�.
+ * Oh! One more thing. Before you start thinking of using linear search let me inform you the array is sorted and rotated.
+ *
+ *** Input format
+ * First line contains an integer T�, number of test cases. Then follows T� test cases. Each test case consists of three lines. The first line contains N�. Second lines contain N� space separated integers and the third line contains an integer x�
+ *
+ ***Output format
+ * Print T� lines, each containing index of x� in A�. Print −1−1 if x� is not present in the array.
+ *
+ ***Input
+ * 3
+ * 9 5 6 7 8 9 10 1 2 3
+ * 10
+ *
+ ***Output
+ * 5
+ * -----------------------------------------------------
+ */
+public class RotateAndFind {
+
+}
