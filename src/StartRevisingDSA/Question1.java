d4064da8ea203510d6ae8be5c91d7e52bbb7d439
Gyanendra-Yadav <gyanendra.yadav@deltatech.gg>
2023-06-19 19:48:05 +0530

    Start solving question of DSA, from today onwards solve 1 question daily. Solving first question.

---
+++ src/StartRevisingDSA/Question1.java
@@ -0,0 +1,20 @@
+package StartRevisingDSA;
+
+/**
+ * Created By:  Gyanendra_Yadav
+ * on 2023-06-19,Jun,2023
+ * in Project: DataStructuresJavaCode
+ *
+ * Rotate Array in Java
+ * You may have been using Java for a while. Do you think a simple Java array question
+ * can be a challenge? Let’s use the following problem to test.
+ * Problem: Rotate an array of n elements to the right by k steps. For example, with n
+ * = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4]. How many different
+ * ways do you know to solve this problem?
+ *
+ */
+public class Question1 {
+    public static void main(String[] args) {
+
+    }
+}
