eb50bb63426e15a699eeed8451882db8875aae2d
Gyanendra-Yadav <gyanendra.yadav@deltatech.gg>
2023-07-27 23:03:52 +0530

    Solving more and more questions

---
+++ src/codingNinjas/Strings/AnagramDifference.java
@@ -0,0 +1,10 @@
+package codingNinjas.Strings;
+
+/**
+ * Created By:  Gyanendra_Yadav
+ * on 2023-07-27,Jul,2023
+ * in Project: DataStructuresJavaCode
+ */
+public class AnagramDifference {
+
+}
---
+++ src/codingNinjas/Strings/EncodeMessage.java
@@ -0,0 +1,31 @@
+package codingNinjas.Strings;
+
+/**
+ * Created By:  Gyanendra_Yadav
+ * on 2023-07-26,Jul,2023
+ * in Project: DataStructuresJavaCode
+ *
+ * https://www.codingninjas.com/studio/guided-paths/data-structures-algorithms/content/118626/offering/1377976?leftPanelTab=0
+ */
+public class EncodeMessage {
+
+    public static void main(String[] args) {
+        System.out.println(encode("aabbbbccc"));
+    }
+
+    public static String encode(String message) {
+        // Write your code here.
+        StringBuilder finalStr= new StringBuilder();
+        int count = 1;
+        for (int i = 0 ; i < message.length() ; i++) {
+            if(message.charAt(i) == message.charAt(i-1)){
+                count++;
+            }
+
+        }
+
+//        finalStr.append(message.charAt(finalStr.length()-1));
+//        finalStr.append(count);
+        return finalStr.toString();
+    }
+}
