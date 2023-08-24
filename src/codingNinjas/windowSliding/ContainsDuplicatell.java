42a650fe636a4b2273efc92a7cc00af0d1df5009
Gyanendra-Yadav <gyanendra.yadav@deltatech.gg>
2023-08-25 00:05:41 +0530

    Solved more questions of programming questions

---
+++ src/codingNinjas/windowSliding/ContainsDuplicatell.java
@@ -0,0 +1,56 @@
+package codingNinjas.windowSliding;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created By:  Gyanendra_Yadav
+ * on 2023-08-24,Aug,2023
+ * in Project: DataStructuresJavaCode
+ */
+public class ContainsDuplicatell {
+    public static void main(String[] args) {
+//        int [] arr={3 ,4 ,9 ,4 ,2};
+//        int [] arr={1 ,2, 3, 4, 5, 1};
+        int [] arr={3 ,2, 0, -4, 7 ,-9, -8, 10, 5, -1};
+        System.out.println(checkDuplicate(arr, arr.length, 4));
+    }
+
+    public static boolean checkDuplicate(int[] arr, int n, int k) {
+        HashMap<Integer, Integer> m = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (m.get(arr[i]) == null)
+                m.put(arr[i], i + 1);
+            else if (m.get(arr[i]) > 0 && Math.abs(m.get(arr[i]) - (i + 1)) <= k)
+                return true;
+            else
+                m.put(arr[i], i + 1);
+        }
+        return false;
+    }
+    /*public static boolean checkDuplicate(int[] arr, int n, int k) {
+        //Base case
+//        if(n<k) return false;
+
+        Map<Integer, Integer> mapCounter;
+        for (int i = 0; i < n-1; i++) {
+            mapCounter=new HashMap<>();
+            mapCounter.put(arr[i], mapCounter.getOrDefault(arr[i],0)+1);
+            int inIndex=i+1;
+            while (inIndex<n && inIndex<=(i+k)){
+                if(mapCounter.containsKey(arr[inIndex])){
+                     if(mapCounter.get(arr[i])>=1){
+                         return true;
+                     }
+                }
+                mapCounter.put(arr[inIndex], mapCounter.getOrDefault(arr[i],0)+1);
+
+                inIndex++;
+            }
+        }
+        return false;
+    }*/
+}
--- src/codingNinjas/windowSliding/CountDistinctElementEveryKSizeWindow.java
+++ src/codingNinjas/windowSliding/CountDistinctElementEveryKSizeWindow.java
@@ -1,8 +1,6 @@
 package codingNinjas.windowSliding;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created By:  Gyanendra_Yadav
@@ -11,7 +9,7 @@ import java.util.Set;
  */
 public class CountDistinctElementEveryKSizeWindow {
     public static void main(String[] args) {
-        ArrayList<Integer> arr = new ArrayList<>();
+        /*ArrayList<Integer> arr = new ArrayList<>();
         arr.add(1);
         arr.add(2);
         arr.add(1);
@@ -21,23 +19,27 @@ public class CountDistinctElementEveryKSizeWindow {
         arr.add(3);
         int k = 3;
 
-        ArrayList<Integer> actualOutput = countDistinctElements(arr, k);
+        ArrayList<Integer> actualOutput = countDistinctElements(arr, k);*/
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println(maximumInAllSubarraysOfSizeK(arr, arr.length, 3));
+
+
     }
 
-    /*public static ArrayList<Integer> countDistinctElements(ArrayList<Integer> arr, int k) {
+   /* public static List<Integer> maximumInAllSubarraysOfSizeK(int[] arr, int n, int k) {
         ArrayList<Integer> resultMax = new ArrayList<>();
         int curr_Max = Integer.MIN_VALUE;
         int overall_Max = Integer.MIN_VALUE;
 
         int index2 = 0;
-        for (int index1 = 0; index1 < (arr.size() - k); index1++) {
+        for (int index1 = 0; index1 < (arr.length - k); index1++) {
             index2=index1+1;
-            while (index2 < arr.size() && (index2 < (index1+k))) {
-               if(arr.get(index1)>arr.get(index2)){
-                   curr_Max=arr.get(index1);
-               }else{
-                   curr_Max=arr.get(index2);
-               }
+            while (index2 < arr.length && (index2 < (index1+k))) {
+                if(arr[index1]>arr[index2]){
+                    curr_Max=arr[index1];
+                }else{
+                    curr_Max=arr[index2];
+                }
                 if (curr_Max>overall_Max){
                     overall_Max=curr_Max;
                 }
@@ -46,27 +48,61 @@ public class CountDistinctElementEveryKSizeWindow {
             resultMax.add(overall_Max);
             index2=0;
         }
-
         return resultMax;
-    }*/
+    }
+    */
+
 
     public static ArrayList<Integer> countDistinctElements(ArrayList<Integer> arr, int k) {
         ArrayList<Integer> resultMax = new ArrayList<>();
 
         for (int index1 = 0; index1 <= (arr.size() - k); index1++) {
-            resultMax.add(findDistinctElement(arr,index1,(index1+k-1)));
+            resultMax.add(findDistinctElement(arr, index1, (index1 + k - 1)));
         }
 
         return resultMax;
     }
 
-    private static int findDistinctElement(ArrayList<Integer> arr, int startIndex, int endIndex){
-        int distinctVale=0;
-        Set<Integer> set=new HashSet<>();
+    private static int findDistinctElement(ArrayList<Integer> arr, int startIndex, int endIndex) {
+        int distinctVale = 0;
+        Set<Integer> set = new HashSet<>();
         for (int i = startIndex; i <= endIndex; i++) {
-            int num=arr.get(i);
+            int num = arr.get(i);
             set.add(num);
         }
         return set.size();
+    }
+
+    public static List<Integer> maximumInAllSubarraysOfSizeKBrutt(int[] arr, int n, int k) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i <= n - k; i++) {
+            list.add(findMaxVal(arr, i, i + k));
+        }
+
+        return list;
+    }
+
+    private static int findMaxVal(int[] arr, int startIndex, int endIndex) {
+        int max = Integer.MIN_VALUE;
+        for (int i = startIndex; i < endIndex; i++) {
+            max = Math.max(max, arr[i]);
+        }
+        return max;
+    }
+
+    public static List<Integer> maximumInAllSubarraysOfSizeK(int[] arr, int n, int k) {
+        int j = 0;
+        List<Integer> ans = new ArrayList<Integer>();
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max, Math.max(arr[j], arr[i]));
+            if (i - j + 1 == k) {
+                ans.add(max);
+                j++;
+                max = Integer.MIN_VALUE;
+            }
+        }
+        return ans;
     }
 }
