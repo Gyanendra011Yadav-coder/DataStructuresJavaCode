be177d5b32ac38e5b42ab2b02a2e197162895a0e
AzureAD\GyanendraYadav <gyanendra.yadav@osttra.com>
2023-11-16 11:21:59 +0530

    Solving another POD of CodingNInjas where need to write number name of passed number.

---
+++ src/codingNinjas/problemOfTheDay/WriteNumberNames.java
@@ -0,0 +1,16 @@
+package codingNinjas.problemOfTheDay;
+
+import java.util.List;
+
+public class WriteNumberNames {
+    public static void main(String[] args) {
+        System.out.println(writeNumberNames(567890));
+    }
+
+    public static String  writeNumberNames(int n) {
+        String result="";
+
+
+        return result;
+    }
+}
