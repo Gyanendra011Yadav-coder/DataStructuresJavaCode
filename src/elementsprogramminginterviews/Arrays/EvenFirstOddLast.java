0cd03916769798f0bf5cd8ef1c38072252e52c0e
AzureAD\GyanendraYadav <gyanendra.yadav@osttra.com>
2024-07-17 09:23:39 +0530

    Started solving Elements of programming interviews problems

---
+++ src/elementsprogramminginterviews/Arrays/EvenFirstOddLast.java
@@ -0,0 +1,38 @@
+package elementsprogramminginterviews.Arrays;
+
+public class EvenFirstOddLast {
+    public static void main(String[] args) {
+        int [] array = {2,3,4,7,8,9,10,11,12,13,14,15};
+//        int [] array = {3,7,9,11,12,14,16,8};
+        reframeArray(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(i + "th element"+ array[i]);
+        }
+    }
+
+    public static void reframeArray(int[] array) {
+        int evenIndex = 0, oddIndex = array.length - 1;
+        while (evenIndex < oddIndex) {
+            if(array[evenIndex] % 2 == 0) {
+                evenIndex++;
+            } else {
+                oddIndex=swap(evenIndex, oddIndex, array);
+                evenIndex++;
+            }
+        }
+    }
+
+    public static int swap(int index1, int index2, int[] array) {
+        int temp=array[index1];
+        while (index1 < index2) {
+            if (array[index2] % 2 == 0) {
+               array[index1]=array[index2];
+               array[index2]=temp;
+               index2--;
+               break;
+            }
+            index2--;
+        }
+        return index2;
+    }
+}
---
+++ src/elementsprogramminginterviews/BitwiseProblems/BasicsOperations.java
@@ -0,0 +1,9 @@
+package elementsprogramminginterviews.BitwiseProblems;
+
+public class BasicsOperations {
+    public static void main(String[] args) {
+        int x = 00;
+        int result = x & -x;
+        System.out.println(result);
+    }
+}
