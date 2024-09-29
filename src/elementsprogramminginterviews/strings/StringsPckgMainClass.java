a058f3466c8735bd5e0605de9c46e7d2e187ddb8
Gyanendra Yadav <gyanu011yadav@gmail.com>
2024-09-29 13:39:45 +0530

    Start Solving String problems

---
+++ src/elementsprogramminginterviews/strings/StringsPckgMainClass.java
@@ -0,0 +1,8 @@
+package elementsprogramminginterviews.strings;
+
+public class StringsPckgMainClass {
+    public static void main(String[] args) {
+        System.out.println("isPalindrome: " + StringProblemsSolving.isPalindrome(" hsgsvghsvgsvsgvsgsvghsv bbshbhssh "));
+
+    }
+}
