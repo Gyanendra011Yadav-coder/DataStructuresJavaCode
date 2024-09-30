d986d81d88246a47f2f2a76773e5313189427ea4
Gyanendra Yadav <gyanu011yadav@gmail.com>
2024-09-30 11:39:21 +0530

    Solving String problem of find and Replace of characters

---
+++ src/elementsprogramminginterviews/strings/ReplaceAndRemove.java
@@ -0,0 +1,42 @@
+package elementsprogramminginterviews.strings;
+
+public class ReplaceAndRemove {
+    private static final char A_CHAR = 'a';
+    private static final char B_CHAR = 'b';
+
+    public static int replaceAndRemove(char[] arrayToUpdate, int charToUpdate) {
+        int updatedChars = 0;
+
+        int write_idx = 0, a_count = 0;
+
+        // Remove all the b characters from the array
+        for (int idx = 0; idx < charToUpdate; idx++) {
+
+            if (arrayToUpdate[idx] != B_CHAR) {
+                arrayToUpdate[write_idx++] = arrayToUpdate[idx];
+            }
+
+            if (arrayToUpdate[idx] == (A_CHAR)) { // keep tracking of a count
+                a_count++;
+            }
+
+        }
+
+        // Here Replacing a with double ds
+        int curr_indx = write_idx - 1;
+        write_idx = write_idx + a_count - 1;
+        updatedChars = write_idx + 1;
+        while (curr_indx >= 0) {
+            if (arrayToUpdate[curr_indx] == (A_CHAR)) {
+                arrayToUpdate[write_idx--] = 'd'; //Adding first d
+                arrayToUpdate[write_idx--] = 'd'; // Adding second d
+            } else {
+                arrayToUpdate[write_idx--] = arrayToUpdate[curr_indx];
+            }
+
+            curr_indx--;
+        }
+
+        return updatedChars;
+    }
+}
--- src/elementsprogramminginterviews/strings/StringsPckgMainClass.java
+++ src/elementsprogramminginterviews/strings/StringsPckgMainClass.java
@@ -1,10 +1,14 @@
 package elementsprogramminginterviews.strings;
 
 public class StringsPckgMainClass {
+    private static char[] charArray = {'a', 'b', 'a', 'c', ' '};
+    private static char[] charArray_2 = {'a', 'c', 'a', 'a', ' ', ' ', ' '};
+
     public static void main(String[] args) {
         System.out.println("isPalindrome: " + StringProblemsSolving.isPalindrome(" hsgsvghsvgsvsgvsgsvghsv bbshbhssh "));
         System.out.println("Convert String to integer: " + InterconvertStringToInt.convertStringToInt("123"));
         System.out.println("Convert String to integer: " + InterconvertStringToInt.convertIntToString(123));
         System.out.println("Convert String to integer: " + ComputerSpreadSheetColumnEncoding.encodeColumn("AAA"));
+        System.out.println("Updated Characters in array is: " + ReplaceAndRemove.replaceAndRemove(charArray_2, 4));
     }
 }
