6cff7c03ae6d82cb3fe9e48f87a70775cf9b508c
Gyanendra Yadav <gyanendra.yadav@osttra.com>
2024-10-06 14:16:35 +0530

    Solving one more problem of generating mnemonics

--- src/elementsprogramminginterviews/strings/StringsPckgMainClass.java
+++ src/elementsprogramminginterviews/strings/StringsPckgMainClass.java
@@ -1,8 +1,12 @@
 package elementsprogramminginterviews.strings;
 
+import java.util.List;
+import java.util.logging.Logger;
+
 public class StringsPckgMainClass {
     private static char[] charArray = {'a', 'b', 'a', 'c', ' '};
     private static char[] charArray_2 = {'a', 'c', 'a', 'a', ' ', ' ', ' '};
+    static Logger log = Logger.getLogger(StringsPckgMainClass.class.getName());
 
     public static void main(String[] args) {
         System.out.println("isPalindrome: " + StringProblemsSolving.isPalindrome(" hsgsvghsvgsvsgvsgsvghsv bbshbhssh "));
@@ -14,5 +18,9 @@ public class StringsPckgMainClass {
         System.out.println("Is Palindrome: " + TestPalindromicity.isPalindromicity("A man, a plan, a canal, Panama"));
         System.out.println("Is Palindrome: " + TestPalindromicity.isPalindromicity("Ray a Ray"));
         System.out.println("Reverse word: " + ReverseWordsInSentence.reverseSentence("Alice likes Bob"));
+        List<String> mnemonics = ComputeMnemonics.computeMnemonics("123");
+        for (String str : mnemonics) {
+            System.out.println(" " + str);
+        }
     }
 }
